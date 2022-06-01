274cdf8c95d6b1eef56cf48a5f8dff7d64cb37bf
gudaoxuri <i@sunisle.org>
2022-06-01 10:58:54 +0800

    RBUM: Fixed a test error.

--- basic/tests/test_rbum_scope_helper.rs
+++ basic/tests/test_rbum_scope_helper.rs
@@ -1,23 +1,23 @@
 use tardis::tokio;
 
-use bios_basic::rbum::helper::rbum_scope_helper::get_match_paths;
+use bios_basic::rbum::helper::rbum_scope_helper::get_pre_paths;
 
 #[tokio::test]
-pub async fn test_get_match_paths() {
-    assert_eq!(get_match_paths(0, ""), Some("%"));
-    assert_eq!(get_match_paths(0, "aaaa"), Some("%"));
+pub async fn test_get_pre_paths() {
+    assert_eq!(get_pre_paths(0, ""), Some("".to_string()));
+    assert_eq!(get_pre_paths(0, "aaaa"), Some("".to_string()));
 
-    assert_eq!(get_match_paths(1, ""), None);
+    assert_eq!(get_pre_paths(1, ""), None);
 
-    assert_eq!(get_match_paths(1, "aaaa"), Some("aaaa%"));
-    assert_eq!(get_match_paths(1, "aaaa/bbbb"), Some("aaaa%"));
-    assert_eq!(get_match_paths(1, "aaaa/bbbb/cccc"), Some("aaaa%"));
+    assert_eq!(get_pre_paths(1, "aaaa"), Some("aaaa".to_string()));
+    assert_eq!(get_pre_paths(1, "aaaa/bbbb"), Some("aaaa".to_string()));
+    assert_eq!(get_pre_paths(1, "aaaa/bbbb/cccc"), Some("aaaa".to_string()));
 
-    assert_eq!(get_match_paths(2, "aaaa"), None);
-    assert_eq!(get_match_paths(2, "aaaa/bbbb"), Some("aaaa/bbbb%"));
-    assert_eq!(get_match_paths(2, "aaaa/bbbb/cccc"), Some("aaaa/bbbb%"));
+    assert_eq!(get_pre_paths(2, "aaaa"), None);
+    assert_eq!(get_pre_paths(2, "aaaa/bbbb"), Some("aaaa/bbbb".to_string()));
+    assert_eq!(get_pre_paths(2, "aaaa/bbbb/cccc"), Some("aaaa/bbbb".to_string()));
 
-    assert_eq!(get_match_paths(3, "aaaa"), None);
-    assert_eq!(get_match_paths(3, "aaaa/bbbb"), Noneo);
-    assert_eq!(get_match_paths(3, "aaaa/bbbb/cccc"), Some("aaaa/bbbb/cccc%"));
+    assert_eq!(get_pre_paths(3, "aaaa"), None);
+    assert_eq!(get_pre_paths(3, "aaaa/bbbb"), None);
+    assert_eq!(get_pre_paths(3, "aaaa/bbbb/cccc"), Some("aaaa/bbbb/cccc".to_string()));
 }
