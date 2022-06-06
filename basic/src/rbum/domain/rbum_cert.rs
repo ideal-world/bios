54fb2a01cbf5b38d1627b02999f6bf71c7f98ed1
gudaoxuri <i@sunisle.org>
2022-06-06 17:25:29 +0800

    RBUM: Code optimization: adding comments (in progress).

--- basic/src/rbum/domain/rbum_cert.rs
+++ basic/src/rbum/domain/rbum_cert.rs
@@ -1,27 +1,58 @@
 use sea_orm::EntityName;
 use tardis::basic::dto::TardisContext;
 use tardis::db::reldb_client::TardisActiveModel;
-use tardis::db::sea_orm::*;
 use tardis::db::sea_orm::prelude::*;
+use tardis::db::sea_orm::*;
 use tardis::db::sea_query::{ColumnDef, Index, IndexCreateStatement, Table, TableCreateStatement};
 
+/// Credential or authentication instance model
+///
+/// Uniform use of cert refers to credentials or authentication
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
 #[sea_orm(table_name = "rbum_cert")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: String,
-    // Specific
+    /// Cert name \
+    /// E.g. username, phone number, app id
     pub ak: String,
+    /// Cert key \
+    /// E.g. password, token, secret key
     pub sk: String,
+    /// Extend information \
+    /// The content and format are set by the upper service itself
     pub ext: String,
+    /// Specifies the start time for the effective date
+    /// 指定生效时间的开始时间
     pub start_time: DateTime,
+    /// Specifies the end time for the effective date
     pub end_time: DateTime,
+    /// Specifies the connection address, mostly for two-party or third-party configurations \
+    /// Information from cert config can be overridden
+    /// E.g. http://localhost:8080/api/v1/
     pub conn_uri: String,
+    /// @see [status](crate::rbum::rbum_enumeration::RbumCertStatusKind)
     pub status: u8,
+    /// Associated cert configuration id
     pub rel_rbum_cert_conf_id: String,
+    /// Associated resource type \
+    /// @see [kind](crate::rbum::rbum_enumeration::RbumCertRelKind)
     pub rel_rbum_kind: u8,
+    /// Associated resource id
+    ///
+    /// Usage examples:
+    ///
+    /// * if rel_rbum_kind == Item
+    ///   - rel_rbum_id same as the rel_rbum_item_id of cert configuration：E.g. Gitlab token
+    ///   - rel_rbum_id different as the rel_rbum_item_id of cert configuration：E.g. User password (the cert configuration is bound to the tenant, and the cert instance corresponds to the user)
+    ///
+    /// * if rel_rbum_kind == Set
+    ///   - E.g. In the Plug-in service, it can be bound to the plug-in instance library
+    ///
+    /// * if rel_rbum_kind == Rel
+    ///  - In the CMDB service, a resource can be sliced (E.g. DB instance), we can specify slice information of association
     pub rel_rbum_id: String,
-    // Basic
+
     pub own_paths: String,
     pub owner: String,
     pub create_time: DateTime,
@@ -62,12 +93,7 @@ impl TardisActiveModel for ActiveModel {
 
     fn create_index_statement() -> Vec<IndexCreateStatement> {
         vec![
-            Index::create().name(&format!("idx-{}-ak", Entity.table_name())).table(Entity)
-                .col(Column::OwnPaths)
-                .col(Column::RelRbumCertConfId)
-                .col(Column::Ak)
-                .unique()
-                .to_owned(),
+            Index::create().name(&format!("idx-{}-ak", Entity.table_name())).table(Entity).col(Column::OwnPaths).col(Column::RelRbumCertConfId).col(Column::Ak).unique().to_owned(),
             Index::create()
                 .name(&format!("idx-{}-{}", Entity.table_name(), Column::RelRbumKind.to_string()))
                 .table(Entity)
