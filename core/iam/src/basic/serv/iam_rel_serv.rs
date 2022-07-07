7350532098481cfcd7f3c4170986b61caad9635c
gudaoxuri <i@sunisle.org>
2022-07-07 18:52:31 +0800

    IAM: Fix the logic of resource and role binding.

--- core/iam/src/basic/serv/iam_rel_serv.rs
+++ core/iam/src/basic/serv/iam_rel_serv.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use tardis::basic::dto::TardisContext;
 use tardis::basic::result::TardisResult;
 use tardis::chrono::{Duration, Utc};
@@ -21,6 +19,13 @@ use crate::iam_enumeration::{IamRelKind, IamResKind};
 
 pub struct IamRelServ;
 
+/// Example of role and resource relationship:
+///
+///  +------------------(3)--------------------+
+///  |                                         |
+/// API1------(1)---- ResMenu1------(2)----- Role1
+///  |                                         |
+///  +--------(4)-----ResMenu2 -----(5)--------+
 impl<'a> IamRelServ {
     pub async fn add_simple_rel(
         rel_kind: &IamRelKind,
@@ -57,8 +62,10 @@ impl<'a> IamRelServ {
         };
         RbumRelServ::add_rel(req, funs, ctx).await?;
         if rel_kind == &IamRelKind::IamResRole {
-            let iam_res = IamResServ::peek_item(
-                from_iam_item_id,
+            let res_id = from_iam_item_id;
+            let role_id = to_iam_item_id;
+            let res = IamResServ::peek_item(
+                res_id,
                 &IamResFilterReq {
                     basic: RbumBasicFilterReq {
                         with_sub_own_paths: true,
@@ -70,60 +77,9 @@ impl<'a> IamRelServ {
                 ctx,
             )
             .await?;
-            if iam_res.kind != IamResKind::Api {
-                return Ok(());
-            }
-            IamResCacheServ::add_or_modify_res_rel(
-                &iam_res.code,
-                &iam_res.method,
-                &IamCacheResRelAddOrModifyReq {
-                    st: if start_timestamp.is_some() { Some(value1) } else { None },
-                    et: if end_timestamp.is_some() { Some(value2) } else { None },
-                    accounts: vec![],
-                    roles: vec![to_iam_item_id.to_string()],
-                    groups: vec![],
-                    apps: vec![],
-                    tenants: vec![],
-                },
-                funs,
-            )
-            .await?;
-        } else if rel_kind == &IamRelKind::IamResApi {
-            // TODO test
-            let iam_api_res = IamResServ::peek_item(
-                from_iam_item_id,
-                &IamResFilterReq {
-                    basic: RbumBasicFilterReq {
-                        with_sub_own_paths: true,
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                },
-                funs,
-                ctx,
-            )
-            .await?;
-            if iam_api_res.kind != IamResKind::Api {
-                return Err(funs.err().conflict("iam_rel", "add", "when add IamResApi kind from item must be api kind"));
-            }
-
-            let rel_res_api_ids = Self::find_from_id_rels(&IamRelKind::IamResApi, true, &iam_api_res.id, None, None, funs, ctx).await?;
-            let rel_res = IamResServ::find_items(
-                &IamResFilterReq {
-                    basic: RbumBasicFilterReq {
-                        ids: Some(rel_res_api_ids),
-                        with_sub_own_paths: true,
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                },
-                None,
-                None,
-                funs,
-                ctx,
-            )
-            .await?;
-            for res in rel_res {
+            if res.kind == IamResKind::Api {
+                // If add is an API resource, the API resource is bound to the role in the cache
+                // See example (3)
                 IamResCacheServ::add_or_modify_res_rel(
                     &res.code,
                     &res.method,
@@ -131,7 +87,7 @@ impl<'a> IamRelServ {
                         st: if start_timestamp.is_some() { Some(value1) } else { None },
                         et: if end_timestamp.is_some() { Some(value2) } else { None },
                         accounts: vec![],
-                        roles: vec![to_iam_item_id.to_string()],
+                        roles: vec![role_id.to_string()],
                         groups: vec![],
                         apps: vec![],
                         tenants: vec![],
@@ -139,7 +95,83 @@ impl<'a> IamRelServ {
                     funs,
                 )
                 .await?;
+            } else {
+                // If add is a menu or element resource
+                // See example (2) / (5)
+                // 1) Find the list of associated API resources
+                let rel_res_api_ids = Self::find_to_id_rels(&IamRelKind::IamResApi, res_id, None, None, funs, ctx).await?;
+                let rel_res_apis = IamResServ::find_items(
+                    &IamResFilterReq {
+                        basic: RbumBasicFilterReq {
+                            ids: Some(rel_res_api_ids),
+                            with_sub_own_paths: true,
+                            ..Default::default()
+                        },
+                        ..Default::default()
+                    },
+                    None,
+                    None,
+                    funs,
+                    ctx,
+                )
+                .await?;
+                // 2) Create bindings of associated API resources to roles in the cache
+                for rel_res_api in rel_res_apis {
+                    IamResCacheServ::add_or_modify_res_rel(
+                        &rel_res_api.code,
+                        &rel_res_api.method,
+                        &IamCacheResRelAddOrModifyReq {
+                            st: if start_timestamp.is_some() { Some(value1) } else { None },
+                            et: if end_timestamp.is_some() { Some(value2) } else { None },
+                            accounts: vec![],
+                            roles: vec![role_id.to_string()],
+                            groups: vec![],
+                            apps: vec![],
+                            tenants: vec![],
+                        },
+                        funs,
+                    )
+                    .await?;
+                }
             }
+        } else if rel_kind == &IamRelKind::IamResApi {
+            let res_api_id = from_iam_item_id;
+            let res_other_id = to_iam_item_id;
+            let res_api = IamResServ::peek_item(
+                res_api_id,
+                &IamResFilterReq {
+                    basic: RbumBasicFilterReq {
+                        with_sub_own_paths: true,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                funs,
+                ctx,
+            )
+            .await?;
+            if res_api.kind != IamResKind::Api {
+                return Err(funs.err().conflict("iam_rel", "add", "when add IamResApi kind from item must be api kind"));
+            }
+            // See example (1) / (4)
+            // Find the list of roles associated with a menu or element resource
+            let rel_role_ids = Self::find_from_id_rels(&IamRelKind::IamResRole, true, res_other_id, None, None, funs, ctx).await?;
+            // Create API bindings to associated roles in the cache
+            IamResCacheServ::add_or_modify_res_rel(
+                &res_api.code,
+                &res_api.method,
+                &IamCacheResRelAddOrModifyReq {
+                    st: if start_timestamp.is_some() { Some(value1) } else { None },
+                    et: if end_timestamp.is_some() { Some(value2) } else { None },
+                    accounts: vec![],
+                    roles: rel_role_ids,
+                    groups: vec![],
+                    apps: vec![],
+                    tenants: vec![],
+                },
+                funs,
+            )
+            .await?;
         }
         Ok(())
     }
@@ -171,8 +203,10 @@ impl<'a> IamRelServ {
         }
         match rel_kind {
             IamRelKind::IamResRole => {
-                let iam_res = IamResServ::peek_item(
-                    from_iam_item_id,
+                let res_id = from_iam_item_id;
+                let role_id = to_iam_item_id;
+                let res = IamResServ::peek_item(
+                    res_id,
                     &IamResFilterReq {
                         basic: RbumBasicFilterReq {
                             with_sub_own_paths: true,
@@ -184,59 +218,15 @@ impl<'a> IamRelServ {
                     ctx,
                 )
                 .await?;
-                if iam_res.kind != IamResKind::Api {
-                    return Ok(());
-                }
-                IamResCacheServ::delete_res_rel(
-                    &iam_res.code,
-                    &iam_res.method,
-                    &IamCacheResRelDeleteReq {
-                        accounts: vec![],
-                        roles: vec![to_iam_item_id.to_string()],
-                        groups: vec![],
-                        apps: vec![],
-                        tenants: vec![],
-                    },
-                    funs,
-                )
-                .await?;
-            }
-            IamRelKind::IamResApi => {
-                // TODO test
-                let iam_api_res = IamResServ::peek_item(
-                    from_iam_item_id,
-                    &IamResFilterReq {
-                        basic: RbumBasicFilterReq {
-                            with_sub_own_paths: true,
-                            ..Default::default()
-                        },
-                        ..Default::default()
-                    },
-                    funs,
-                    ctx,
-                )
-                .await?;
-                if iam_api_res.kind != IamResKind::Api {
-                    return Err(funs.err().conflict("iam_rel", "delete", "when delete IamResApi kind from item must be api kind"));
-                }
-                let rel_menu_or_ele_res_ids = IamResServ::find_from_id_rel_roles(&IamRelKind::IamResApi, true, &iam_api_res.id, None, None, funs, ctx).await?;
-                let mut rel_roles_ids: HashMap<String, i64> = HashMap::new();
-                for rel_menu_or_ele_res_id in rel_menu_or_ele_res_ids {
-                    IamResServ::find_from_id_rel_roles(&IamRelKind::IamResRole, true, &rel_menu_or_ele_res_id, None, None, funs, ctx).await?.into_iter().for_each(|rel_role_id| {
-                        rel_roles_ids.entry(rel_role_id).and_modify(|count| *count += 1).or_insert(0);
-                    });
-                }
-                let only_one_rel_res_role_ids = rel_roles_ids.into_iter().filter(|(_, v)| *v == 1).map(|(k, _)| k).collect::<Vec<String>>();
-                if only_one_rel_res_role_ids.is_empty() {
-                    return Ok(());
-                }
-                for only_one_rel_res_role_id in only_one_rel_res_role_ids {
+                if res.kind == IamResKind::Api {
+                    // If delete is an API resource, remove the API resource binding to the role from the cache
+                    // See example (3)
                     IamResCacheServ::delete_res_rel(
-                        &iam_api_res.code,
-                        &iam_api_res.method,
+                        &res.code,
+                        &res.method,
                         &IamCacheResRelDeleteReq {
                             accounts: vec![],
-                            roles: vec![only_one_rel_res_role_id.to_string()],
+                            roles: vec![role_id.to_string()],
                             groups: vec![],
                             apps: vec![],
                             tenants: vec![],
@@ -244,7 +234,117 @@ impl<'a> IamRelServ {
                         funs,
                     )
                     .await?;
+                } else {
+                    // If delete is a menu or element resource
+                    // See example (2) / (5)
+                    // 1) Find the list of associated API resources (ready to remove the binding to the role from the cache)
+                    let rel_res_api_ids = Self::find_to_id_rels(&IamRelKind::IamResApi, res_id, None, None, funs, ctx).await?;
+                    for rel_res_api_id in rel_res_api_ids {
+                        // 2) If the associated API resource is explicitly associated with a role, it cannot be removed
+                        if Self::exist_rels(&IamRelKind::IamResRole, &rel_res_api_id, role_id, funs, ctx).await? {
+                            continue;
+                        }
+                        // 3) Find the list of menu or element resources associated with the associated API resource (indirect relationship)
+                        let rel_res_other_ids = Self::find_from_id_rels(&IamRelKind::IamResApi, true, &rel_res_api_id, None, None, funs, ctx)
+                            .await?
+                            .into_iter()
+                            // 4) Exclude own Id
+                            .filter(|r| r != res_id)
+                            .collect::<Vec<String>>();
+                        // 5) If these associated menus or element resources are explicitly associated with a role, they cannot be removed
+                        for rel_res_other_id in rel_res_other_ids {
+                            if Self::exist_rels(&IamRelKind::IamResRole, &rel_res_other_id, role_id, funs, ctx).await? {
+                                break;
+                            }
+                        }
+                        // 6) Get information about the resources
+                        let rel_res_api = IamResServ::peek_item(
+                            &rel_res_api_id,
+                            &IamResFilterReq {
+                                basic: RbumBasicFilterReq {
+                                    with_sub_own_paths: true,
+                                    ..Default::default()
+                                },
+                                ..Default::default()
+                            },
+                            funs,
+                            ctx,
+                        )
+                        .await?;
+                        // 7) Remove API resources from binding to roles in the cache
+                        IamResCacheServ::delete_res_rel(
+                            &rel_res_api.code,
+                            &rel_res_api.method,
+                            &IamCacheResRelDeleteReq {
+                                accounts: vec![],
+                                roles: vec![role_id.to_string()],
+                                groups: vec![],
+                                apps: vec![],
+                                tenants: vec![],
+                            },
+                            funs,
+                        )
+                        .await?;
+                    }
                 }
+            }
+            IamRelKind::IamResApi => {
+                let res_api_id = from_iam_item_id;
+                let res_other_id = to_iam_item_id;
+                let res_api = IamResServ::peek_item(
+                    res_api_id,
+                    &IamResFilterReq {
+                        basic: RbumBasicFilterReq {
+                            with_sub_own_paths: true,
+                            ..Default::default()
+                        },
+                        ..Default::default()
+                    },
+                    funs,
+                    ctx,
+                )
+                .await?;
+                if res_api.kind != IamResKind::Api {
+                    return Err(funs.err().conflict("iam_rel", "delete", "when delete IamResApi kind from item must be api kind"));
+                }
+                // See example (1) / (4)
+                // 1) Find the list of roles associated with a menu or element resource (ready to remove the binding to the API resource from the cache)
+                let rel_role_ids = Self::find_from_id_rels(&IamRelKind::IamResRole, true, res_other_id, None, None, funs, ctx).await?;
+                let mut remove_role_ids = Vec::new();
+                for rel_role_id in rel_role_ids {
+                    // 2) If an API resource is explicitly associated with a role, it cannot be removed
+                    if Self::exist_rels(&IamRelKind::IamResRole, res_api_id, &rel_role_id, funs, ctx).await? {
+                        continue;
+                    }
+                    // 3) Find the list of resources associated with the associated role (indirect relationship)
+                    let rel_res_ids = Self::find_to_id_rels(&IamRelKind::IamResRole, &rel_role_id, None, None, funs, ctx)
+                        .await?
+                        .into_iter()
+                        // 4) Exclude own Id
+                        .filter(|r| r != res_other_id)
+                        .collect::<Vec<String>>();
+                    // 5) If these associated resources are explicitly associated with API resources, they cannot be removed
+                    for rel_res_id in rel_res_ids {
+                        if Self::exist_rels(&IamRelKind::IamResApi, res_api_id, &rel_res_id, funs, ctx).await? {
+                            break;
+                        }
+                    }
+                    remove_role_ids.push(rel_role_id);
+                }
+                // 6) Remove API resources from binding to roles in the cache
+                IamResCacheServ::delete_res_rel(
+                    &res_api.code,
+                    &res_api.method,
+                    &IamCacheResRelDeleteReq {
+                        accounts: vec![],
+                        roles: remove_role_ids,
+                        groups: vec![],
+                        apps: vec![],
+                        tenants: vec![],
+                    },
+                    funs,
+                )
+                .await?;
             }
             IamRelKind::IamAccountRole => {
                 IamIdentCacheServ::delete_tokens_and_contexts_by_account_id(from_iam_item_id, funs).await?;
