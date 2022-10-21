c6bc0972e88297c348bd2614e71d2172969f6e4a
何乐天 <869759838@qq.com>
2022-10-21 18:15:04 +0800

    update document

--- core/iam/src/console_passport/api/iam_cp_cert_api.rs
+++ core/iam/src/console_passport/api/iam_cp_cert_api.rs
@@ -288,9 +288,9 @@ impl IamCpCertLdapApi {
         TardisResp::ok(resp)
     }
 
-    /// <p>bind username password cert by ldap</p>
-    /// if ak param is None then create new userpwd cert
-    /// else bind with ldap cert
+    /// bind username password cert by ldap \
+    /// if ak param is None then create new userpwd cert \
+    /// else bind with ldap cert 
     /// name-password -ldap login
     #[oai(path = "/bind-or-create-userpwd", method = "put")]
     async fn bind_or_create_user_pwd_cert_by_ldap(&self, login_req: Json<IamCpUserPwdBindWithLdapReq>) -> TardisApiResult<IamAccountInfoWithUserPwdAkResp> {
