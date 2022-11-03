cbf3f490ee8692d3d8041c04df29a6c183c2ca83
李节龙 <17743125563@163.com>
2022-11-03 14:54:48 +0800

    iam: fix IamAccountSummaryAggResp add owner and own_paths

--- core/iam/src/basic/dto/iam_account_dto.rs
+++ core/iam/src/basic/dto/iam_account_dto.rs
@@ -132,6 +132,8 @@ pub struct IamAccountSummaryAggResp {
     pub id: String,
     pub name: String,
 
+    pub own_paths: String,
+    pub owner: String,
     pub create_time: DateTime<Utc>,
     pub update_time: DateTime<Utc>,
 
