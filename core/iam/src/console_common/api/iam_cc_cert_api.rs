d8a62a11f5169daf1a5ab5c349233915855e6ae2
何乐天 <869759838@qq.com>
2022-12-07 16:23:09 +0800

    1

--- core/iam/src/console_common/api/iam_cc_cert_api.rs
+++ core/iam/src/console_common/api/iam_cc_cert_api.rs
@@ -1,9 +1,12 @@
+use crate::basic::dto::iam_account_dto::IamAccountBoneResp;
+use crate::basic::serv::iam_cert_serv::IamCertServ;
+use crate::iam_constants;
+use bios_basic::rbum::dto::rbum_cert_dto::RbumCertSummaryWithSkResp;
 use tardis::web::context_extractor::TardisContextExtractor;
 use tardis::web::poem::web::Path;
 use tardis::web::poem_openapi;
 use tardis::web::poem_openapi::param::Query;
-use tardis::web::web_resp::{TardisApiResult, TardisPage};
-use crate::basic::dto::iam_account_dto::IamAccountBoneResp;
+use tardis::web::web_resp::{TardisApiResult, TardisPage, TardisResp};
 
 pub struct IamCcCertApi;
 
@@ -11,15 +14,23 @@ pub struct IamCcCertApi;
 #[poem_openapi::OpenApi(prefix_path = "/cc/cert", tag = "bios_basic::ApiTag::Common")]
 impl IamCcCertApi {
     /// Find Accounts
-    #[oai(path = "/:kind", method = "get")]
-    async fn paginate(
+    #[oai(path = "/:account_id", method = "get")]
+    async fn get_cert_by_kind_supplier(
         &self,
-        kind: Path<String>,
-        supplier: Query<supplier>,
+        account_id: Path<String>,
+        kind: Query<String>,
+        tenant_id: Query<Option<String>>,
+        supplier: Query<Option<String>>,
         ctx: TardisContextExtractor,
-    ) -> TardisApiResult<TardisPage<IamAccountBoneResp>> {
-
-    }
-
+    ) -> TardisApiResult<RbumCertSummaryWithSkResp> {
+        let funs = iam_constants::get_tardis_inst();
+        let supplier = supplier.0.unwrap_or_default();
+        let conf_id = if let Ok(conf_id) = IamCertServ::get_cert_conf_id_by_kind_supplier(&kind.0, &supplier.clone(), tenant_id.0, &funs).await {
+            Some(conf_id)
+        } else {
+            None
+        };
+        let cert = IamCertServ::get_cert_by_relrubmid_kind_supplier(&account_id.0, &kind.0, vec![supplier], conf_id, &funs, &ctx.0).await?;
+        TardisResp::ok(cert)
     }
 }
