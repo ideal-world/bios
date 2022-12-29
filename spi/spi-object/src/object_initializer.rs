922568e750f145151d2f1587fdfe48679e95c2c4
gudaoxuri <i@sunisle.org>
2022-12-28 18:13:22 -0800

    Complete the development and testing of the spi-object service.

---
+++ spi/spi-object/src/object_initializer.rs
@@ -0,0 +1,42 @@
+use bios_basic::spi::{api::spi_ci_bs_api, dto::spi_bs_dto::SpiBsCertResp, spi_funs::SpiBsInst, spi_initializer};
+use tardis::{
+    basic::{dto::TardisContext, error::TardisError, result::TardisResult},
+    web::web_server::TardisWebServer,
+    TardisFuns, TardisFunsInst,
+};
+
+use crate::{
+    api::ci::api::object_obj_api,
+    object_constants::{self, DOMAIN_CODE},
+    serv,
+};
+
+pub async fn init(web_server: &TardisWebServer) -> TardisResult<()> {
+    let mut funs = TardisFuns::inst_with_db_conn(DOMAIN_CODE.to_string(), None);
+    funs.begin().await?;
+    let ctx = spi_initializer::init(DOMAIN_CODE, &funs).await?;
+    init_db(&funs, &ctx).await?;
+    funs.commit().await?;
+    init_api(web_server).await
+}
+
+async fn init_db(funs: &TardisFunsInst, ctx: &TardisContext) -> TardisResult<()> {
+    spi_initializer::add_kind(object_constants::SPI_S3_KIND_CODE, funs, ctx).await?;
+    Ok(())
+}
+
+async fn init_api(web_server: &TardisWebServer) -> TardisResult<()> {
+    web_server.add_module(DOMAIN_CODE, (spi_ci_bs_api::SpiCiBsApi, object_obj_api::ObjectCiObjApi)).await;
+    Ok(())
+}
+
+pub async fn init_fun(bs_cert: SpiBsCertResp, ctx: &TardisContext, mgr: bool) -> TardisResult<SpiBsInst> {
+    match bs_cert.kind_code.as_str() {
+        #[cfg(feature = "spi-s3")]
+        object_constants::SPI_S3_KIND_CODE => serv::s3::object_s3_initializer::init(&bs_cert, ctx, mgr).await,
+        _ => Err(TardisError::not_implemented(
+            &format!("Backend service kind {} does not exist or SPI feature is not enabled", bs_cert.kind_code),
+            "406-rbum-*-enum-init-error",
+        ))?,
+    }
+}
---
+++ spi/spi-object/src/serv/s3/object_s3_initializer.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+use bios_basic::spi::{dto::spi_bs_dto::SpiBsCertResp, spi_funs::SpiBsInst, spi_initializer};
+use tardis::{
+    basic::{dto::TardisContext, error::TardisError, result::TardisResult},
+    os::os_client::TardisOSClient,
+    TardisFuns,
+};
+
+pub async fn init(bs_cert: &SpiBsCertResp, ctx: &TardisContext, _: bool) -> TardisResult<SpiBsInst> {
+    let ext = TardisFuns::json.str_to_json(&bs_cert.ext)?;
+    let client = TardisOSClient::init(
+        "s3",
+        ext.get("endpoint").unwrap().as_str().unwrap(),
+        ext.get("ak").unwrap().as_str().unwrap(),
+        ext.get("sk").unwrap().as_str().unwrap(),
+        ext.get("region").unwrap().as_str().unwrap(),
+        if let Some(default_bucket) = ext.get("default_bucket") {
+            default_bucket.as_str().unwrap()
+        } else {
+            ""
+        },
+    )?;
+    let mut ext = HashMap::new();
+    if !bs_cert.private {
+        let bucket_name_prefix = spi_initializer::common::get_isolation_flag_from_context(ctx);
+        let resp = client.bucket_create_simple(&format!("{}-pri", bucket_name_prefix), true).await;
+        if !resp.is_ok() && resp.err().unwrap().code != "409" {
+            return Err(TardisError::internal_error(&format!("Bucket {}-pri creation failed", bucket_name_prefix), ""));
+        }
+        let resp = client.bucket_create_simple(&format!("{}-pub", bucket_name_prefix), false).await;
+        if !resp.is_ok() && resp.err().unwrap().code != "409" {
+            return Err(TardisError::internal_error(&format!("Bucket {}-pub creation failed", bucket_name_prefix), ""));
+        }
+        spi_initializer::common::set_isolation_flag_to_ext(&bucket_name_prefix, &mut ext);
+    };
+    Ok(SpiBsInst { client: Box::new(client), ext })
+}
