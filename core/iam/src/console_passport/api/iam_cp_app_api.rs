2cf2b4aab2b7d092ee61ff66e8cee2cdad3d5a7a
RWDai <869759838@qq.com>
2022-12-30 10:19:39 +0800

    add_cp_app_api

--- core/iam/src/console_passport/api/iam_cp_app_api.rs
+++ core/iam/src/console_passport/api/iam_cp_app_api.rs
@@ -1,22 +1,22 @@
-use bios_basic::rbum::dto::rbum_filer_dto::RbumBasicFilterReq;
+use crate::basic::dto::iam_app_dto::IamAppSummaryResp;
+use crate::basic::dto::iam_filer_dto::IamAppFilterReq;
+use crate::basic::serv::iam_app_serv::IamAppServ;
+use crate::iam_constants;
+use crate::iam_enumeration::IamRelKind;
+use bios_basic::rbum::dto::rbum_filer_dto::{RbumBasicFilterReq, RbumItemRelFilterReq};
+use bios_basic::rbum::rbum_enumeration::RbumRelFromKind;
+use bios_basic::rbum::serv::rbum_item_serv::RbumItemCrudOperation;
 use tardis::web::context_extractor::TardisContextExtractor;
 use tardis::web::poem_openapi;
 use tardis::web::poem_openapi::param::Query;
 use tardis::web::web_resp::{TardisApiResult, TardisPage, TardisResp};
 
-use bios_basic::rbum::serv::rbum_item_serv::RbumItemCrudOperation;
+pub struct IamCpAppApi;
 
-use crate::basic::dto::iam_app_dto::IamAppSummaryResp;
-use crate::basic::dto::iam_filer_dto::IamAppFilterReq;
-use crate::basic::serv::iam_app_serv::IamAppServ;
-use crate::iam_constants;
-
-pub struct IamCcAppApi;
-
-/// Common Console App API
-#[poem_openapi::OpenApi(prefix_path = "/cc/app", tag = "bios_basic::ApiTag::Common")]
-impl IamCcAppApi {
-    /// Find Apps
+/// Passport Console App API
+#[poem_openapi::OpenApi(prefix_path = "/cp/app", tag = "bios_basic::ApiTag::Passport")]
+impl IamCpAppApi {
+    /// Find Apps by ctx.owner(account_id)
     #[oai(path = "/", method = "get")]
     async fn paginate(
         &self,
@@ -39,6 +39,14 @@ impl IamCcAppApi {
                     enabled: Some(true),
                     ..Default::default()
                 },
+                rel: Some(RbumItemRelFilterReq {
+                    rel_by_from: false,
+                    is_left: false,
+                    tag: Some(IamRelKind::IamAccountApp.to_string()),
+                    from_rbum_kind: Some(RbumRelFromKind::Item),
+                    rel_item_id: Some(ctx.0.owner.clone()),
+                    ..Default::default()
+                }),
                 ..Default::default()
             },
             page_number.0,
