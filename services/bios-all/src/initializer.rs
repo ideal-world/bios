fd3b44496837600cead2dc7a1768c845375b637b
gudaoxuri <i@sunisle.org>
2023-02-28 03:01:03 -0800

    global: Upgrade tardis version..

--- services/bios-all/src/initializer.rs
+++ services/bios-all/src/initializer.rs
@@ -4,5 +4,13 @@ use tardis::web::web_server::TardisWebServer;
 pub async fn init(web_server: &TardisWebServer) -> TardisResult<()> {
     bios_auth::auth_initializer::init(web_server).await?;
     bios_iam::iam_initializer::init(web_server).await?;
+
+    bios_spi_cache::cache_initializer::init(web_server).await?;
+    bios_spi_kv::kv_initializer::init(web_server).await?;
+    bios_spi_log::log_initializer::init(web_server).await?;
+    bios_spi_object::object_initializer::init(web_server).await?;
+    bios_spi_reldb::reldb_initializer::init(web_server).await?;
+    bios_spi_search::search_initializer::init(web_server).await?;
+    bios_spi_stats::stats_initializer::init(web_server).await?;
     Ok(())
 }
