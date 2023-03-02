307af7c16bcfec02c66311b3dd6169dde0337510
gudaoxuri <i@sunisle.org>
2023-03-02 02:27:57 -0800

    spi-stats: Complete core coding and testing.

--- spi/spi-stats/tests/test_stats_metric.rs
+++ spi/spi-stats/tests/test_stats_metric.rs
@@ -1,7 +1,11 @@
+use std::time::Duration;
+
 use bios_basic::test::test_http_client::TestHttpClient;
 use bios_spi_stats::dto::stats_query_dto::StatsQueryMetricsResp;
 use tardis::basic::result::TardisResult;
+use tardis::chrono::Utc;
 use tardis::serde_json::{json, Value};
+use tardis::tokio::time::sleep;
 use tardis::web::web_resp::Void;
 
 pub async fn test(client: &mut TestHttpClient) -> TardisResult<()> {
@@ -42,12 +46,14 @@ pub async fn test(client: &mut TestHttpClient) -> TardisResult<()> {
 
     let _: Void = client.put("/ci/record/fact/req/batch/load", &data).await;
 
+    test_metric_query_check(client).await?;
     test_metric_query(client).await?;
 
     Ok(())
 }
 
-pub async fn test_metric_query(client: &mut TestHttpClient) -> TardisResult<()> {
+pub async fn test_metric_query_check(client: &mut TestHttpClient) -> TardisResult<()> {
+    // fact not exist error
     assert_eq!(
         client
             .put_resp::<Value, StatsQueryMetricsResp>(
@@ -65,6 +71,7 @@ pub async fn test_metric_query(client: &mut TestHttpClient) -> TardisResult<()> 
         "404-spi-stats-metric-query"
     );
 
+    // dimension in group not exist error
     assert_eq!(
         client
             .put_resp::<Value, StatsQueryMetricsResp>(
@@ -82,6 +89,7 @@ pub async fn test_metric_query(client: &mut TestHttpClient) -> TardisResult<()> 
         "404-spi-stats-metric-query"
     );
 
+    // fact column in select not exist error
     assert_eq!(
         client
             .put_resp::<Value, StatsQueryMetricsResp>(
@@ -99,6 +107,64 @@ pub async fn test_metric_query(client: &mut TestHttpClient) -> TardisResult<()> 
         "404-spi-stats-metric-query"
     );
 
+    // select function not exist error
+    assert_eq!(
+        client
+            .put_resp::<Value, StatsQueryMetricsResp>(
+                "/ci/metric",
+                &json!({
+                    "from":"req",
+                    "select":[{"code":"act_hours","fun":"xxx"},{"code":"plan_hours","fun":"sum"}],
+                    "group":[{"code":"source"}],
+                    "start_time":"2023-01-01T12:00:00.000Z",
+                    "end_time":"2023-02-01T12:00:00.000Z"
+                }),
+            )
+            .await
+            .code,
+        "400"
+    );
+
+    // group function not exist error
+    assert_eq!(
+        client
+            .put_resp::<Value, StatsQueryMetricsResp>(
+                "/ci/metric",
+                &json!({
+                    "from":"req",
+                    "select":[{"code":"act_hours","fun":"sum"},{"code":"plan_hours","fun":"sum"}],
+                    "group":[{"code":"source","time_window":"xxx"}],
+                    "start_time":"2023-01-01T12:00:00.000Z",
+                    "end_time":"2023-02-01T12:00:00.000Z"
+                }),
+            )
+            .await
+            .code,
+        "400"
+    );
+
+    // group function not legal (time window function only for date/datetime type) error
+    assert_eq!(
+        client
+            .put_resp::<Value, StatsQueryMetricsResp>(
+                "/ci/metric",
+                &json!({
+                    "from":"req",
+                    "select":[{"code":"act_hours","fun":"sum"},{"code":"plan_hours","fun":"sum"}],
+                    "group":[{"code":"source","time_window":"day"}],
+                    "start_time":"2023-01-01T12:00:00.000Z",
+                    "end_time":"2023-02-01T12:00:00.000Z"
+                }),
+            )
+            .await
+            .code,
+        "404-spi-stats-metric-query"
+    );
+    Ok(())
+}
+
+pub async fn test_metric_query(client: &mut TestHttpClient) -> TardisResult<()> {
+    // test simple one dimension
     let resp: StatsQueryMetricsResp = client
         .put(
             "/ci/metric",
@@ -114,10 +180,194 @@ pub async fn test_metric_query(client: &mut TestHttpClient) -> TardisResult<()> 
     assert_eq!(resp.from, "req");
     assert_eq!(resp.show_names.len(), 3);
     assert_eq!(resp.group.as_object().unwrap().len(), 4);
-    assert_eq!(resp.group.as_object().unwrap()["_"]["act_hours_sum"], 100);
-    assert_eq!(resp.group.as_object().unwrap()["_"]["plan_hours_sum"], 200);
-    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["act_hours_sum"], 80);
-    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["plan_hours_sum"], 160);
+    assert_eq!(resp.group.as_object().unwrap()[""]["act_hours__sum"], 100);
+    assert_eq!(resp.group.as_object().unwrap()[""]["plan_hours__sum"], 200);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["act_hours__sum"], 80);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["plan_hours__sum"], 160);
+
+    // test simple two dimensions
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"sum"},{"code":"plan_hours","fun":"max"}],
+                "group":[{"code":"source"},{"code":"status"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time":"2023-02-01T12:00:00.000Z"
+            }),
+        )
+        .await;
+    assert_eq!(resp.from, "req");
+    assert_eq!(resp.show_names.len(), 4);
+    assert_eq!(resp.show_names["act_hours__sum"].as_str(), "实例工时");
+    assert_eq!(resp.show_names["plan_hours__max"].as_str(), "计划工时");
+    assert_eq!(resp.show_names["status__"].as_str(), "状态");
+    assert_eq!(resp.show_names["source__"].as_str(), "来源");
+    assert_eq!(resp.group.as_object().unwrap().len(), 4);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"][""]["act_hours__sum"], 80);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"][""]["plan_hours__max"], 20);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["close"]["act_hours__sum"], 10);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["close"]["plan_hours__max"], 20);
+
+    // test tree dimensions with multiple values
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"avg"},{"code":"plan_hours","fun":"avg"}],
+                "group":[{"code":"source"},{"code":"status"},{"code":"tag"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time":"2023-02-01T12:00:00.000Z"
+            }),
+        )
+        .await;
+    assert_eq!(resp.from, "req");
+    assert_eq!(resp.show_names.len(), 5);
+    assert_eq!(resp.show_names["act_hours__avg"].as_str(), "实例工时");
+    assert_eq!(resp.show_names["plan_hours__avg"].as_str(), "计划工时");
+    assert_eq!(resp.show_names["status__"].as_str(), "状态");
+    assert_eq!(resp.show_names["source__"].as_str(), "来源");
+    assert_eq!(resp.show_names["tag__"].as_str(), "标签");
+    assert_eq!(resp.group.as_object().unwrap().len(), 4);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"][""]["act_hours__avg"], 80);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"][""]["plan_hours__avg"], 20);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["close"]["act_hours__avg"], 10);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["close"]["plan_hours__avg"], 20);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["close"]["f1"]["act_hours__avg"], 10);
+    assert_eq!(resp.group.as_object().unwrap()["hangzhou"]["close"]["f2"]["plan_hours__avg"], 20);
+
+    // test two dimensions with time window
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"avg"},{"code":"plan_hours","fun":"avg"}],
+                "group":[{"code":"ct","time_window":"day"},{"code":"status"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time":"2023-02-01T12:00:00.000Z"
+            }),
+        )
+        .await;
+
+    // test two dimensions with limit
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"avg"},{"code":"plan_hours","fun":"avg"}],
+                "group":[{"code":"ct","time_window":"date"},{"code":"status"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time":"2023-02-01T12:00:00.000Z",
+                "limit":2
+            }),
+        )
+        .await;
+
+    // test two dimensions with order
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"avg"},{"code":"plan_hours","fun":"avg"}],
+                "group":[{"code":"ct","time_window":"date"},{"code":"status"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time":"2023-02-01T12:00:00.000Z",
+                "order": [{"code":"ct","asc": false}]
+            }),
+        )
+        .await;
+
+    // test tree dimensions with having
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"sum"},{"code":"plan_hours","fun":"sum"}],
+                "group":[{"code":"ct","time_window":"date"},{"code":"status"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time":"2023-02-01T12:00:00.000Z",
+                "having": [{"code":"act_hours","fun": "sum", "op":">", "value":30}]
+            }),
+        )
+        .await;
+
+    // test two dimensions with all
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"sum"},{"code":"plan_hours","fun":"sum"}],
+                "group":[{"code":"ct","time_window":"date"},{"code":"status"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time":"2023-02-01T12:00:00.000Z",
+                "order": [{"code":"ct","asc": false}],
+                "having": [{"code":"act_hours","fun": "sum", "op":">", "value":30}],
+                "limit":2
+            }),
+        )
+        .await;
+
+    // test where
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"sum"},{"code":"plan_hours","fun":"sum"}],
+                "group":[{"code":"ct","time_window":"date"},{"code":"status"}],
+                "where":[{"code":"act_hours", "op":">", "value":10},{"code":"ct", "op":"!=", "value":"01", "time_window":"day"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time":"2023-02-01T12:00:00.000Z"
+            }),
+        )
+        .await;
+
+    // test with delete record
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"sum"},{"code":"plan_hours","fun":"sum"}],
+                "group":[{"code":"source"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time": Utc::now().to_rfc3339()
+            }),
+        )
+        .await;
+    assert_eq!(client.delete_resp("/ci/record/fact/req/r011").await.code, "200");
+    sleep(Duration::from_millis(100)).await;
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"sum"},{"code":"plan_hours","fun":"sum"}],
+                "group":[{"code":"source"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time": Utc::now().to_rfc3339()
+            }),
+        )
+        .await;
+    let resp: StatsQueryMetricsResp = client
+        .put(
+            "/ci/metric",
+            &json!({
+                "from":"req",
+                "select":[{"code":"act_hours","fun":"sum"},{"code":"plan_hours","fun":"sum"}],
+                "group":[{"code":"source"}],
+                "start_time":"2023-01-01T12:00:00.000Z",
+                "end_time":"2023-02-01T12:00:00.000Z"
+            }),
+        )
+        .await;
 
     Ok(())
 }
