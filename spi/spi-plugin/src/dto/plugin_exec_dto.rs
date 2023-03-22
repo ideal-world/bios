509de6b179613c731bd0aa9a155f0946530e0d49
李节龙 <17743125563@163.com>
2023-03-22 21:18:13 +0800

    spi-plugin: fix exec body modify Value

--- spi/spi-plugin/src/dto/plugin_exec_dto.rs
+++ spi/spi-plugin/src/dto/plugin_exec_dto.rs
@@ -1,12 +1,12 @@
 use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
-use tardis::web::poem_openapi;
+use tardis::{serde_json::Value, web::poem_openapi};
 
 #[derive(poem_openapi::Object, Serialize, Deserialize, Debug)]
 pub struct PluginExecReq {
     pub header: Option<HashMap<String, String>>,
-    pub body: Option<HashMap<String, String>>,
+    pub body: Option<Value>,
 }
 
 #[derive(poem_openapi::Object, Serialize, Deserialize, Debug, Clone)]
