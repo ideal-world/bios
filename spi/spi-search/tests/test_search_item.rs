4af679dda7a7f6a8c836df6086b8a88da156d52f
李节龙 <17743125563@163.com>
2023-03-24 18:13:41 +0800

    spi-search:fix

--- spi/spi-search/tests/test_search_item.rs
+++ spi/spi-search/tests/test_search_item.rs
@@ -28,7 +28,7 @@ pub async fn test(client: &mut TestHttpClient) -> TardisResult<()> {
                 "own_paths":"t001",
                 "create_time":"2022-11-26T23:23:59.000Z",
                 "update_time": "2022-11-27T01:20:20.000Z",
-                "ext":{"version":"1.1"}
+                "ext":{"version":"1.1","xxx":0}
             }),
         )
         .await;
@@ -39,7 +39,7 @@ pub async fn test(client: &mut TestHttpClient) -> TardisResult<()> {
             &json!({
                 "title": "全局#号搜索",
                 "content": "在任意信息流（FEED，包含需求、任务、缺陷、文档等）中输入#号时出现一个跟随光标的快捷搜索小窗口，可以输入编号或内容模糊匹配对应的数据，如果存在，则可以选中对应的数据并显示在文本中。",
-                "ext":{"start_time":"2022-11-25T14:23:20.000Z","end_time":"2022-11-30T14:23:20.000Z","rel_accounts":["acc01","acc02"]}
+                "ext":{"start_time":"2022-11-25T14:23:20.000Z","end_time":"2022-11-30T14:23:20.000Z","rel_accounts":["acc01","acc02"],"version":"1.x"}
             }),
         )
         .await;
@@ -109,6 +109,8 @@ pub async fn test(client: &mut TestHttpClient) -> TardisResult<()> {
         .await;
     assert_eq!(search_result.total_size, 3);
     assert_eq!(search_result.records[0].key, "001");
+    assert_eq!(search_result.records[0].ext.get("xxx").unwrap().as_i64().unwrap(), 0);
+    assert_eq!(search_result.records[0].ext.get("version").unwrap().as_str().unwrap(), "1.x");
 
     // Basic search
     let search_result: TardisPage<SearchItemSearchResp> = client
