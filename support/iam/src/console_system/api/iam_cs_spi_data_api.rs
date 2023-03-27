7ad4fd28dba7d98870045754d9f91fb5e2db6985
soberHoa <869759838@qq.com>
2023-03-27 16:55:06 +0800

    1

--- support/iam/src/console_system/api/iam_cs_spi_data_api.rs
+++ support/iam/src/console_system/api/iam_cs_spi_data_api.rs
@@ -11,6 +11,7 @@ use tardis::TardisFunsInst;
 use crate::basic::serv::iam_account_serv::IamAccountServ;
 use crate::basic::serv::iam_app_serv::IamAppServ;
 use crate::basic::serv::iam_tenant_serv::IamTenantServ;
+#[cfg(feature = "spi_kv")]
 use crate::basic::serv::spi_client::spi_kv_client::SpiKvClient;
 use crate::iam_constants;
 
