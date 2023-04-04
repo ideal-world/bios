2cf125053dcfa11e104f9703c6db6784bb41f799
gudaoxuri <i@sunisle.org>
2023-04-04 07:40:49 -0700

    enhance-wasm: Init.

--- support/enhance-wasm/src/modules/crypto/crypto.rs
+++ support/enhance-wasm/src/modules/crypto/crypto.rs
@@ -7,7 +7,7 @@ use lazy_static::lazy_static;
 use crate::{
     basic::error::TardisError,
     helper::{
-        crypto_key::TardisCryptoKey,
+        crypto_base64, crypto_key,
         crypto_sm2_4::{TardisCryptoSm2, TardisCryptoSm2PrivateKey, TardisCryptoSm2PublicKey, TardisCryptoSm4},
         http,
     },
@@ -30,6 +30,10 @@ pub(crate) async fn init(serv_url: &str) -> Result<(), JsValue> {
     let mut serv_pub_key = SERV_PUB_KEY.write().unwrap();
     *serv_pub_key = Some(TardisCryptoSm2 {}.new_public_key_from_public_key(&pub_key)?);
 
+    init_fd_key()
+}
+
+pub(crate) fn init_fd_key() -> Result<(), JsValue> {
     let sm_obj = TardisCryptoSm2 {};
     let pri_key = sm_obj.new_private_key()?;
     let pub_key = sm_obj.new_public_key(&pri_key)?;
@@ -43,9 +47,8 @@ pub fn encrypt(body: &str, need_crypto_req: bool, need_crypto_resp: bool) -> Res
     let serv_pub_key = serv_pub_key.as_ref().unwrap();
 
     let (body, encrypt_key) = if need_crypto_req && need_crypto_resp {
-        let key = TardisCryptoKey {};
-        let sm4_key = key.rand_16_hex()?;
-        let sm4_iv = key.rand_16_hex()?;
+        let sm4_key = crypto_key::rand_16_hex()?;
+        let sm4_iv = crypto_key::rand_16_hex()?;
         let encrypt_body =
             TardisCryptoSm4 {}.encrypt_cbc(body, &sm4_key, &sm4_iv).map_err(|e| TardisError::bad_request(&format!("[Auth] Encrypted request: body encrypt error:{e}"), ""))?;
 
@@ -57,9 +60,8 @@ pub fn encrypt(body: &str, need_crypto_req: bool, need_crypto_resp: bool) -> Res
             .map_err(|e| TardisError::bad_request(&format!("[Auth] Encrypted request: key encrypt error:{e}"), ""))?;
         (encrypt_body, encrypt_key)
     } else if need_crypto_req {
-        let key = TardisCryptoKey {};
-        let sm4_key = key.rand_16_hex()?;
-        let sm4_iv = key.rand_16_hex()?;
+        let sm4_key = crypto_key::rand_16_hex()?;
+        let sm4_iv = crypto_key::rand_16_hex()?;
         let encrypt_body =
             TardisCryptoSm4 {}.encrypt_cbc(body, &sm4_key, &sm4_iv).map_err(|e| TardisError::bad_request(&format!("[Auth] Encrypted request: body encrypt error:{e}"), ""))?;
 
@@ -73,6 +75,7 @@ pub fn encrypt(body: &str, need_crypto_req: bool, need_crypto_resp: bool) -> Res
         let encrypt_key = serv_pub_key.encrypt(&format!("{fd_pub_key}")).map_err(|e| TardisError::bad_request(&format!("[Auth] Encrypted request: key encrypt error:{e}"), ""))?;
         (body.to_string(), encrypt_key)
     };
+    let encrypt_key = crypto_base64::encode(&encrypt_key);
     let resp = EncryptResp { body, key: encrypt_key };
     Ok(serde_wasm_bindgen::to_value(&resp)?)
 }
@@ -81,7 +84,8 @@ pub fn decrypt(encrypt_body: &str, encrypt_key: &str) -> Result<String, JsValue>
     let fd_pri_key = FD_SM2_KEYS.read().unwrap();
     let fd_pri_key = &fd_pri_key.as_ref().unwrap().1;
 
-    let key = fd_pri_key.decrypt(encrypt_key)?;
+    let encrypt_key = crypto_base64::decode(encrypt_key)?;
+    let key = fd_pri_key.decrypt(&encrypt_key)?;
     let key = key.split(" ").collect::<Vec<&str>>();
     let sm4_key = key[0];
     let sm4_iv = key[1];
