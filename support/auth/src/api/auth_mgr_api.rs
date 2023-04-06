2660f74dd935c5b78a1464a01db16ce3cee1f13f
gudaoxuri <i@sunisle.org>
2023-04-06 00:28:16 -0700

    auth: Remove cache api.

--- support/auth/src/api/auth_mgr_api.rs
+++ support/auth/src/api/auth_mgr_api.rs
@@ -9,10 +9,10 @@ pub struct MgrApi;
 /// Management API
 #[poem_openapi::OpenApi(prefix_path = "/auth/mgr")]
 impl MgrApi {
-    /// Fetch Cached Resources
-    #[oai(path = "/cache/res", method = "get")]
-    async fn fetch_cache_res(&self) -> TardisApiResult<Value> {
-        let result = auth_mgr_serv::fetch_cache_res()?;
-        TardisResp::ok(result)
-    }
+    // /// Fetch Cached Resources
+    // #[oai(path = "/cache/res", method = "get")]
+    // async fn fetch_cache_res(&self) -> TardisApiResult<Value> {
+    //     let result = auth_mgr_serv::fetch_cache_res()?;
+    //     TardisResp::ok(result)
+    // }
 }
--- support/auth/src/serv/auth_mgr_serv.rs
+++ support/auth/src/serv/auth_mgr_serv.rs
@@ -1,12 +1,6 @@
-use tardis::{basic::result::TardisResult, serde_json::Value, TardisFuns};
+use tardis::{basic::result::TardisResult, TardisFuns};
 
 use crate::{auth_config::AuthConfig, auth_constants::DOMAIN_CODE};
-
-use super::auth_res_serv;
-
-pub(crate) fn fetch_cache_res() -> TardisResult<Value> {
-    auth_res_serv::get_res_json()
-}
 
 pub(crate) async fn has_double_auth(account_id: &str) -> TardisResult<bool> {
     let cache_client = TardisFuns::cache_by_module_or_default(DOMAIN_CODE);
