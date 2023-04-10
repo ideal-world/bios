a88da91f68a76f9cf381df1ad6cdbe19a720c43d
RWDai <rwdai@foxmail.com>
2023-04-09 21:58:41 -0700

    add

--- support/auth/tests/test_auth_encrypt.rs
+++ support/auth/tests/test_auth_encrypt.rs
@@ -1,6 +1,37 @@
-async fn mock_req(method: &str, path: &str, query: &str, headers: Vec<(&str, &str)>,) -> AuthResp {
+use std::collections::HashMap;
+
+use bios_auth::{
+    auth_config::AuthConfig,
+    auth_constants::DOMAIN_CODE,
+    dto::auth_kernel_dto::{AuthReq, AuthResp},
+};
+use tardis::chrono::{Duration, Utc};
+use tardis::{
+    basic::{dto::TardisContext, result::TardisResult},
+    log::info,
+    serde_json::Value,
+    web::{web_client::TardisWebClient, web_resp::TardisResp},
+    TardisFuns,
+};
+
+async fn mock_req(method: &str, path: &str, body: Value, mut headers: Vec<(&str, &str)>, pub_key: &str, need_crypto_req: bool, need_crypto_resp: bool) -> AuthResp {
     let web_client = TardisWebClient::init(1).unwrap();
     info!(">>>>[Request]| path:{}, query:{}, headers:{:#?}", path, query, headers);
+    let config = TardisFuns::cs_config::<AuthConfig>(DOMAIN_CODE);
+    let data = if need_crypto_req {
+        let pub_key = TardisFuns::crypto.sm2.new_public_key_from_public_key(&pub_key).unwrap();
+
+        let sm4_key = TardisFuns::crypto.key.rand_16_hex().unwrap();
+        let sm4_iv = TardisFuns::crypto.key.rand_16_hex().unwrap();
+
+        let data = TardisFuns::crypto.sm4.encrypt_cbc(&body, &sm4_key, &sm4_iv).unwrap();
+
+        let pub_key = pub_key.encrypt(&format!("{sm4_key} {sm4_iv}")).unwrap();
+        headers.push((&config.head_key_crypto, &TardisFuns::crypto.base64.encode(&pub_key)));
+        data
+    } else {
+        body.to_string()
+    };
     let hashmap_query = if query.is_empty() {
         HashMap::new()
     } else {
@@ -23,7 +54,7 @@ async fn mock_req(method: &str, path: &str, query: &str, headers: Vec<(&str, &st
                 host: "".to_string(),
                 port: 80,
                 headers: headers.iter().map(|(k, v)| (k.to_string(), v.to_string())).collect::<HashMap<String, String>>(),
-                body: None,
+                body: Some(data),
             },
             None,
         )
@@ -32,9 +63,17 @@ async fn mock_req(method: &str, path: &str, query: &str, headers: Vec<(&str, &st
         .body
         .unwrap();
     info!("<<<<[Request]|path:{}, query:{}, headers:{:#?}, result:{:#?}", path, query, headers, result);
+    //todo need_crypto_resp
+    result.data.unwrap()
+}
+async fn init_get_pub_key() -> TardisResult<String> {
+    //frontend init sm2
+    let pri_key = TardisFuns::crypto.sm2.new_private_key()?;
+    let pub_key = TardisFuns::crypto.sm2.new_public_key(&pri_key)?;
+
+    let web_client = TardisWebClient::init(1).unwrap();
+    let result: TardisResp<String> = web_client.get(&format!("https://localhost:8080/{DOMAIN_CODE}/auth/crypto/key"), None).await.unwrap().body.unwrap();
     result.data.unwrap()
 }
 
-pub async fn test_encrypt() -> TardisResult<()> {
-
-}
+pub async fn test_encrypt() -> TardisResult<()> {}
