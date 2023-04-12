85fe71d4044b1133f6c21a391bab8071810398ca
ZzIsGod1019 <1498852723@qq.com>
2023-04-11 19:27:16 -0700

    add ci interface

---
+++ support/iam/src/console_interface/api/iam_ci_app_api.rs
@@ -0,0 +1,72 @@
+use crate::basic::dto::iam_app_dto::IamAppAggModifyReq;
+use crate::basic::serv::iam_app_serv::IamAppServ;
+use crate::basic::serv::iam_role_serv::IamRoleServ;
+use crate::iam_constants::{self, RBUM_SCOPE_LEVEL_APP};
+use bios_basic::process::task_processor::TaskProcessor;
+use tardis::web::context_extractor::TardisContextExtractor;
+use tardis::web::poem_openapi;
+use tardis::web::poem_openapi::param::Path;
+use tardis::web::poem_openapi::payload::Json;
+use tardis::web::web_resp::{TardisApiResult, TardisResp, Void};
+
+pub struct IamCiAppApi;
+
+/// # Interface Console Manage Cert API
+///
+/// Allow Management Of aksk (an authentication method between applications)
+#[poem_openapi::OpenApi(prefix_path = "/ci/app", tag = "bios_basic::ApiTag::Interface")]
+impl IamCiAppApi {
+    /// Modify Current App
+    ///
+    /// When code = 202, the return value is the asynchronous task id
+    #[oai(path = "/", method = "put")]
+    async fn modify(&self, modify_req: Json<IamAppAggModifyReq>, ctx: TardisContextExtractor) -> TardisApiResult<Option<String>> {
+        let mut funs = iam_constants::get_tardis_inst();
+        funs.begin().await?;
+        IamAppServ::modify_app_agg(&IamAppServ::get_id_by_ctx(&ctx.0, &funs)?, &modify_req, &funs, &ctx.0).await?;
+        funs.commit().await?;
+        if let Some(task_id) = TaskProcessor::get_task_id_with_ctx(&ctx.0)? {
+            TardisResp::accepted(Some(task_id))
+        } else {
+            TardisResp::ok(None)
+        }
+    }
+
+    /// Batch add Role Rel Account
+    #[oai(path = "/role/:id/account/batch/:account_ids", method = "put")]
+    async fn batch_add_rel_account(&self, id: Path<String>, account_ids: Path<String>, ctx: TardisContextExtractor) -> TardisApiResult<Void> {
+        let mut funs = iam_constants::get_tardis_inst();
+        funs.begin().await?;
+        let app_id = IamAppServ::get_id_by_ctx(&ctx.0, &funs)?;
+        let split = account_ids.0.split(',').collect::<Vec<_>>();
+        for s in split {
+            IamAppServ::add_rel_account(&app_id, s, true, &funs, &ctx.0).await?;
+            IamRoleServ::add_rel_account(&id.0, s, Some(RBUM_SCOPE_LEVEL_APP), &funs, &ctx.0).await?;
+        }
+        funs.commit().await?;
+        TardisResp::ok(Void {})
+    }
+
+    /// Batch delete Role Rel Account
+    #[oai(path = "/role/:id/account/batch/:account_ids", method = "delete")]
+    async fn batch_delete_rel_account(&self, id: Path<String>, account_ids: Path<String>, ctx: TardisContextExtractor) -> TardisApiResult<Void> {
+        let mut funs = iam_constants::get_tardis_inst();
+        funs.begin().await?;
+        let split = account_ids.0.split(',').collect::<Vec<_>>();
+        for s in split {
+            IamRoleServ::delete_rel_account(&id.0, s, Some(RBUM_SCOPE_LEVEL_APP), &funs, &ctx.0).await?;
+        }
+        funs.commit().await?;
+        TardisResp::ok(Void {})
+    }
+
+    /// Delete Role Rel Account
+    #[oai(path = "/role/:id/account/:account_id", method = "delete")]
+    async fn delete_rel_account(&self, id: Path<String>, account_id: Path<String>, ctx: TardisContextExtractor) -> TardisApiResult<Void> {
+        let mut funs = iam_constants::get_tardis_inst();
+        funs.begin().await?;
+        IamRoleServ::delete_rel_account(&id.0, &account_id.0, Some(RBUM_SCOPE_LEVEL_APP), &funs, &ctx.0).await?;
+        funs.commit().await?;
+        TardisResp::ok(Void {})
+    }
+}
---
+++ support/iam/src/console_interface/api/iam_ci_system_api.rs
@@ -0,0 +1,62 @@
+use crate::basic::dto::iam_res_dto::IamResAggAddReq;
+use crate::basic::dto::iam_set_dto::IamSetCateAddReq;
+use crate::basic::serv::iam_res_serv::IamResServ;
+use crate::basic::serv::iam_set_serv::IamSetServ;
+use crate::iam_constants;
+use crate::iam_enumeration::IamSetKind;
+use bios_basic::rbum::rbum_config::RbumConfigApi;
+use tardis::TardisFuns;
+use tardis::web::context_extractor::TardisContextExtractor;
+use tardis::web::poem_openapi;
+use tardis::web::poem_openapi::payload::Json;
+use tardis::web::web_resp::{TardisApiResult, TardisResp};
+
+pub struct IamCiSystemApi;
+
+/// # Interface Console Manage Cert API
+///
+/// Allow Management Of aksk (an authentication method between applications)
+#[poem_openapi::OpenApi(prefix_path = "/ci/system", tag = "bios_basic::ApiTag::Interface")]
+impl IamCiSystemApi {
+    #[oai(path = "/res", method = "post")]
+    async fn add(&self, mut add_req: Json<IamResAggAddReq>, ctx: TardisContextExtractor) -> TardisApiResult<String> {
+        let mut funs = iam_constants::get_tardis_inst();
+        funs.begin().await?;
+        let set_id = IamSetServ::get_default_set_id_by_ctx(&IamSetKind::Res, &funs, &ctx.0).await?;
+        let result = IamResServ::add_res_agg(&mut add_req.0, &set_id, &funs, &ctx.0).await?;
+        funs.commit().await?;
+        TardisResp::ok(result)
+    }
+
+    /// Add Res Cate
+    #[oai(path = "/res/cate", method = "post")]
+    async fn add_cate(&self, add_req: Json<IamSetCateAddReq>, ctx: TardisContextExtractor) -> TardisApiResult<String> {
+        let mut funs = iam_constants::get_tardis_inst();
+        funs.begin().await?;
+        let set_cate_sys_code_node_len = funs.rbum_conf_set_cate_sys_code_node_len();
+        let api_sys_code = TardisFuns::field.incr_by_base36(&String::from_utf8(vec![b'0'; set_cate_sys_code_node_len]).ok().unwrap()).unwrap();
+        let set_id = IamSetServ::get_default_set_id_by_ctx(&IamSetKind::Res, &funs, &ctx.0).await?;
+        let rbum_parent_cate_id = if add_req.0.rbum_parent_cate_id.is_none() {
+            Some(IamSetServ::get_cate_id_with_sys_code(set_id.as_str(), api_sys_code.as_str(), &funs, &ctx.0).await?)
+        } else {
+            add_req.0.rbum_parent_cate_id
+        };
+        let result = IamSetServ::add_set_cate(
+            &set_id,
+            &IamSetCateAddReq {
+                name: add_req.0.name,
+                scope_level: add_req.0.scope_level,
+                bus_code: add_req.0.bus_code,
+                icon: add_req.0.icon,
+                sort: add_req.0.sort,
+                ext: add_req.0.ext,
+                rbum_parent_cate_id,
+            },
+            &funs,
+            &ctx.0,
+        )
+        .await?;
+        funs.commit().await?;
+        TardisResp::ok(result)
+    }
+}
