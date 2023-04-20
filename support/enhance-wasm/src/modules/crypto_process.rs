9375060c5b4b7191f20b0b8da179dde13b003286
RWDai <rwdai@foxmail.com>
2023-04-19 19:31:18 -0700

    dont need crypt

--- support/enhance-wasm/src/modules/crypto_process.rs
+++ support/enhance-wasm/src/modules/crypto_process.rs
@@ -63,6 +63,12 @@ pub fn encrypt(method: &str, uri: &str, body: &str) -> TardisResult<EncryptResp>
 }
 
 pub fn do_encrypt(body: &str, need_crypto_req: bool, need_crypto_resp: bool) -> TardisResult<EncryptResp> {
+    if !need_crypto_req && !need_crypto_resp {
+        return Ok(EncryptResp {
+            body: body.to_string(),
+            additional_headers: HashMap::new(),
+        });
+    }
     let config = STABLE_CONFIG.read().unwrap();
     let config = config.as_ref().unwrap();
     let serv_pub_key = &config.serv_pub_key;
@@ -215,6 +221,13 @@ mod tests {
                         need_crypto_resp: false,
                         need_double_auth: false,
                     },
+                    Api {
+                        action: "get".to_string(),
+                        uri: "iam/ct/none".to_string(),
+                        need_crypto_req: false,
+                        need_crypto_resp: false,
+                        need_double_auth: false,
+                    },
                 ],
                 login_req_method: "".to_string(),
                 login_req_paths: vec![],
@@ -320,6 +333,11 @@ mod tests {
         // Encrypt
         let mock_req_body = "中台经过几年“滚雪球”的发展或是资本地运作，已是个“庞然大物”，是到了“减肥”，“减负”的时候。一言以避之：解构中台，让他融合到更大的IT能力共享架构中，把共享交给开放平台，把技术还给技术平台，让中台专注于领域服务及事件 。";
         let encrypt_req = encrypt("delete", "iam/ct/resp/xxx", mock_req_body).unwrap();
+        assert_eq!(encrypt_req.body, mock_req_body);
+        assert!(encrypt_req.additional_headers.is_empty());
+
+        let mock_req_body = "中台经过几年“滚雪球”的发展或是资本地运作，已是个“庞然大物”，是到了“减肥”，“减负”的时候。一言以避之：解构中台，让他融合到更大的IT能力共享架构中，把共享交给开放平台，把技术还给技术平台，让中台专注于领域服务及事件 。";
+        let encrypt_req = encrypt("get", "iam/ct/none", mock_req_body).unwrap();
         assert_eq!(encrypt_req.body, mock_req_body);
         assert!(encrypt_req.additional_headers.is_empty());
     }
