a536f1b6eecbe68bf144212c4ca4689ee3e950e9
LiJieLong <17743125563@163.com>
2023-05-18 02:21:40 -0700

    schedule:fix

--- middleware/schedule/src/schedule_initializer.rs
+++ middleware/schedule/src/schedule_initializer.rs
@@ -1,11 +1,8 @@
-use bios_basic::{
-    rbum::{rbum_config::RbumConfig, rbum_initializer},
-    spi::{dto::spi_bs_dto::SpiBsCertResp, spi_constants, spi_funs::SpiBsInst, spi_initializer},
-};
+use bios_basic::spi::{dto::spi_bs_dto::SpiBsCertResp, spi_constants, spi_funs::SpiBsInst, spi_initializer};
 use tardis::{
     basic::{dto::TardisContext, result::TardisResult},
     web::web_server::TardisWebServer,
-    TardisFuns, TardisFunsInst,
+    TardisFuns,
 };
 
 use crate::{api::ci::schedule_ci_job_api, schedule_constants::DOMAIN_CODE, serv::schedule_job_serv};
@@ -13,16 +10,17 @@ use crate::{api::ci::schedule_ci_job_api, schedule_constants::DOMAIN_CODE, serv:
 pub async fn init(web_server: &TardisWebServer) -> TardisResult<()> {
     let mut funs = TardisFuns::inst_with_db_conn(DOMAIN_CODE.to_string(), None);
     funs.begin().await?;
-    let ctx = spi_initializer::init(DOMAIN_CODE, &funs).await?;
+    let ctx = TardisContext {
+        own_paths: "".to_string(),
+        owner: "".to_string(),
+        ak: "_".to_string(),
+        roles: vec![],
+        groups: vec![],
+        ext: Default::default(),
+    };
     schedule_job_serv::init(&funs, &ctx).await?;
-    init_db(&funs, &ctx).await?;
     funs.commit().await?;
     init_api(web_server).await
-}
-
-async fn init_db(funs: &TardisFunsInst, ctx: &TardisContext) -> TardisResult<()> {
-    spi_initializer::add_kind(spi_constants::SPI_PG_KIND_CODE, funs, ctx).await?;
-    Ok(())
 }
 
 async fn init_api(web_server: &TardisWebServer) -> TardisResult<()> {
