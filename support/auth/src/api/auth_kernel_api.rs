ba17dad66ee83602d28a2d81b462f67c5a1b1dd7
LiJieLong <17743125563@163.com>
2023-07-10 00:00:13 -0700

    auth:fix get token context.

--- support/auth/src/api/auth_kernel_api.rs
+++ support/auth/src/api/auth_kernel_api.rs
@@ -3,9 +3,13 @@ use tardis::log::trace;
 use tardis::serde_json::Value;
 use tardis::web::poem::Request;
 use tardis::web::poem_openapi;
+use tardis::web::poem_openapi::param::Query;
 use tardis::web::poem_openapi::payload::Json;
 use tardis::web::web_resp::{TardisApiResult, TardisResp};
+use tardis::TardisFuns;
 
+use crate::auth_config::AuthConfig;
+use crate::auth_constants::DOMAIN_CODE;
 use crate::dto::auth_kernel_dto::{AuthReq, AuthResp, MixAuthResp, SignWebHookReq};
 use crate::serv::clients::spi_log_client::{LogParamContent, SpiLogClient};
 use crate::serv::{auth_kernel_serv, auth_res_serv};
@@ -13,10 +17,10 @@ use crate::serv::{auth_kernel_serv, auth_res_serv};
 #[derive(Clone)]
 pub struct AuthApi;
 
-/// Auth API
+/// Auth API 身份验证API
 #[poem_openapi::OpenApi(prefix_path = "/auth")]
 impl AuthApi {
-    /// Auth
+    /// Auth 身份验证
     #[oai(path = "/", method = "put")]
     async fn auth(&self, req: Json<AuthReq>, request: &Request) -> TardisApiResult<AuthResp> {
         let result = auth_kernel_serv::auth(&mut req.0.clone(), false).await?;
@@ -37,7 +41,7 @@ impl AuthApi {
         TardisResp::ok(result)
     }
 
-    // mix apis
+    // mix apis 解析混合api
     #[oai(path = "/apis", method = "put")]
     async fn apis(&self, req: Json<AuthReq>, request: &Request) -> TardisApiResult<MixAuthResp> {
         let result = auth_kernel_serv::parse_mix_req(req.0.clone()).await?;
@@ -58,7 +62,7 @@ impl AuthApi {
         TardisResp::ok(result)
     }
 
-    /// fetch server config
+    /// fetch server config 获取服务器配置
     #[oai(path = "/apis", method = "get")]
     async fn fetch_server_config(&self, request: &Request) -> TardisApiResult<Value> {
         let result = auth_res_serv::get_apis_json()?;
@@ -77,7 +81,16 @@ impl AuthApi {
         TardisResp::ok(result)
     }
 
-    /// sign webhook
+    /// get token context  获取token上下文
+    #[oai(path = "/token", method = "get")]
+    async fn get_token_context(&self, token: Query<String>, app_id: Query<Option<String>>) -> TardisApiResult<String> {
+        let config = TardisFuns::cs_config::<AuthConfig>(DOMAIN_CODE);
+        let cache_client = TardisFuns::cache_by_module_or_default(DOMAIN_CODE);
+        let result = auth_kernel_serv::get_token_context(&token.0, &app_id.0.unwrap_or("".to_string()), config, cache_client).await?;
+        TardisResp::ok(TardisFuns::crypto.base64.encode(&TardisFuns::json.obj_to_string(&result)?))
+    }
+
+    /// sign webhook 签名webhook
     #[oai(path = "/sign/webhook", method = "put")]
     async fn sign_webhook(&self, req: Json<SignWebHookReq>) -> TardisApiResult<String> {
         let result = auth_kernel_serv::sign_webhook_ak(&req.0).await?;
