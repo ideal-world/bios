027ee24316e2bc38811cfebb75f1ba9fac37030e
RWDai <27391645+RWDai@users.noreply.github.com>
2023-07-24 01:21:10 -0700

    update

--- support/iam/src/console_common/api/iam_cc_platform_api.rs
+++ support/iam/src/console_common/api/iam_cc_platform_api.rs
@@ -4,6 +4,7 @@ use tardis::web::poem_openapi;
 use tardis::web::web_resp::{TardisApiResult, TardisResp};
 
 use crate::basic::dto::iam_platform_dto::IamPlatformConfigResp;
+use crate::basic::serv::iam_account_serv::IamAccountServ;
 use crate::basic::serv::iam_platform_serv::IamPlatformServ;
 use crate::iam_constants;
 
@@ -17,7 +18,8 @@ impl IamCcPlatformApi {
     #[oai(path = "/", method = "get")]
     async fn get(&self, ctx: TardisContextExtractor) -> TardisApiResult<IamPlatformConfigResp> {
         let funs = iam_constants::get_tardis_inst();
-        let result = IamPlatformServ::get_platform_config_agg(&funs, &ctx.0).await?;
+        let new_ctx = IamAccountServ::new_context_if_account_is_global(&ctx.0, &funs).await?;
+        let result = IamPlatformServ::get_platform_config_agg(&funs, &new_ctx).await?;
         ctx.0.execute_task().await?;
         TardisResp::ok(result)
     }
