081b71d3e2045b95e3ff0eb0cc05b4b25d7352fd
ljl <17743125563@163.com>
2023-07-25 11:25:44 +0800

    iam:mail and sms add async.

--- support/iam/src/basic/serv/iam_cert_mail_vcode_serv.rs
+++ support/iam/src/basic/serv/iam_cert_mail_vcode_serv.rs
@@ -405,7 +405,7 @@ impl IamCertMailVCodeServ {
         let resp = IamCertServ::get_kernel_cert(account_id, &IamCertKernelKind::MailVCode, funs, ctx).await;
         match resp {
             Ok(cert) => {
-                let _ = MailClient::send_pwd(&cert.ak, pwd, funs).await;
+                let _ = MailClient::async_send_pwd(&cert.ak, pwd, funs, ctx).await;
             }
             Err(_) => info!("mail pwd not found"),
         }
