f6a669c66870f8275d1e4f5e7f40d3bbb6299811
ljl <17743125563@163.com>
2023-07-26 10:27:10 +0800

    rbum:fix tree.

--- basic/src/rbum/serv/rbum_set_serv.rs
+++ basic/src/rbum/serv/rbum_set_serv.rs
@@ -249,6 +249,7 @@ impl RbumSetServ {
                     ..Default::default()
                 },
                 rel_rbum_set_id: Some(rbum_set_id.to_string()),
+                table_rbum_set_cate_is_left: Some(true),
                 sys_code_query_kind: filter.sys_code_query_kind.clone(),
                 sys_code_query_depth: filter.sys_code_query_depth,
                 rel_rbum_set_cate_sys_codes: filter.sys_codes.clone(),
