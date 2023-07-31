c133f568a06cf9c711c06f13143feff11f57b884
ZzIsGod1019 <1498852723@qq.com>
2023-07-30 22:49:52 -0700

    flow: feat test

--- middleware/flow/tests/test_flow_api.rs
+++ middleware/flow/tests/test_flow_api.rs
@@ -1,12 +1,19 @@
 use std::env;
 use std::time::Duration;
 
+use bios_basic::rbum::rbum_config::RbumConfig;
+use bios_basic::rbum::serv::rbum_kind_serv::RbumKindServ;
+use bios_basic::spi::dto::spi_bs_dto::SpiBsAddReq;
+use bios_basic::spi::spi_constants;
 use bios_basic::test::init_rbum_test_container;
 use bios_basic::test::test_http_client::TestHttpClient;
 use bios_mw_flow::{flow_constants, flow_initializer};
+use bios_spi_kv::{kv_constants, kv_initializer};
 use tardis::basic::dto::TardisContext;
+use tardis::basic::field::TrimString;
 use tardis::basic::result::TardisResult;
 use tardis::tokio::time::sleep;
+use tardis::web::web_resp::Void;
 use tardis::{testcontainers, tokio, TardisFuns};
 
 mod test_flow_scenes_fsm;
@@ -23,6 +30,7 @@ async fn test_flow_api() -> TardisResult<()> {
 
     let web_server = TardisFuns::web_server();
     flow_initializer::init(web_server).await.unwrap();
+    init_spi_kv().await?;
 
     tokio::spawn(async move {
         web_server.start().await.unwrap();
@@ -33,12 +41,12 @@ async fn test_flow_api() -> TardisResult<()> {
     let mut client = TestHttpClient::new("https://localhost:8080/flow".to_string());
 
     test_flow_scenes_fsm::test(&mut client).await?;
-    init_data().await?;
+    init_flow_data().await?;
 
     Ok(())
 }
 
-async fn init_data() -> TardisResult<()> {
+async fn init_flow_data() -> TardisResult<()> {
     let funs = flow_constants::get_tardis_inst();
     flow_initializer::truncate_data(&funs).await?;
     let ctx = TardisContext {
@@ -50,5 +58,55 @@ async fn init_data() -> TardisResult<()> {
         ..Default::default()
     };
     flow_initializer::init_rbum_data(&funs, &ctx).await?;
+    Ok(())
+}
+
+async fn init_spi_kv() -> TardisResult<()> {
+    // Initialize RBUM
+    bios_basic::rbum::rbum_initializer::init(kv_constants::DOMAIN_CODE, RbumConfig::default()).await?;
+
+    let web_server = TardisFuns::web_server();
+    // Initialize SPI KV
+    kv_initializer::init(web_server).await.unwrap();
+
+    tokio::spawn(async move {
+        web_server.start().await.unwrap();
+    });
+
+    sleep(Duration::from_millis(500)).await;
+
+    let funs = TardisFuns::inst_with_db_conn(kv_constants::DOMAIN_CODE.to_string(), None);
+    let kind_id = RbumKindServ::get_rbum_kind_id_by_code(spi_constants::SPI_PG_KIND_CODE, &funs).await?.unwrap();
+    let ctx = TardisContext {
+        own_paths: "".to_string(),
+        ak: "".to_string(),
+        roles: vec![],
+        groups: vec![],
+        owner: "".to_string(),
+        ..Default::default()
+    };
+
+    let mut client = TestHttpClient::new(format!("https://localhost:8080/{}", kv_constants::DOMAIN_CODE));
+
+    client.set_auth(&ctx)?;
+
+    let bs_id: String = client
+        .post(
+            "/ci/manage/bs",
+            &SpiBsAddReq {
+                name: TrimString("test-spi".to_string()),
+                kind_id: TrimString(kind_id),
+                conn_uri: env::var("TARDIS_FW.DB.URL").unwrap(),
+                ak: TrimString("".to_string()),
+                sk: TrimString("".to_string()),
+                ext: "{\"max_connections\":20,\"min_connections\":10}".to_string(),
+                private: false,
+                disabled: None,
+            },
+        )
+        .await;
+
+    let _: Void = client.put(&format!("/ci/manage/bs/{}/rel/app001", bs_id), &Void {}).await;
+
     Ok(())
 }
