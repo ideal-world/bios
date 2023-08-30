25065cba2e5274dc3b571862e39c60f2b104f43d
4t145 <u4t145@163.com>
2023-08-30 17:31:56 +0800

    remove all unsafe codes

--- basic/src/spi/macros.rs
+++ basic/src/spi/macros.rs
@@ -52,7 +52,7 @@ macro_rules! spi_dispatch_service {
         $(
             $(#[$attr])*
             pub async fn $service($($arg: $type,)* funs: &tardis::TardisFunsInst, ctx: &tardis::basic::dto::TardisContext) -> $ret {
-                let inst = funs.init(ctx, $mgr, $init).await?;
+                let inst = funs.init(ctx, $mgr, $init).await?.as_ref();
                 $crate::spi_dispatch_function!($service, funs, ctx, inst, @dispatch: $dispatch, @args: {$($arg),*})
             }
         )*
