08e1f351b0a2f660250f813a75ce4b7b4f260940
ljl <17743125563@163.com>
2023-09-05 10:24:19 +0800

    iam:fix app set.

---
+++ support/iam/src/console_common/api/iam_cc_app_set_api.rs
@@ -0,0 +1,84 @@
+use bios_basic::rbum::dto::rbum_set_dto::RbumSetTreeResp;
+use tardis::web::context_extractor::TardisContextExtractor;
+use tardis::web::poem_openapi;
+use tardis::web::poem_openapi::{param::Path, param::Query, payload::Json};
+use tardis::web::web_resp::{TardisApiResult, TardisResp, Void};
+
+use bios_basic::rbum::dto::rbum_filer_dto::RbumSetTreeFilterReq;
+use bios_basic::rbum::dto::rbum_set_item_dto::RbumSetItemDetailResp;
+use bios_basic::rbum::rbum_enumeration::RbumSetCateLevelQueryKind;
+
+use crate::basic::dto::iam_set_dto::{IamSetCateAddReq, IamSetCateModifyReq, IamSetItemAddReq, IamSetItemWithDefaultSetAddReq};
+use crate::basic::serv::iam_cert_serv::IamCertServ;
+use crate::basic::serv::iam_set_serv::IamSetServ;
+use crate::iam_constants;
+use crate::iam_enumeration::IamSetKind;
+use bios_basic::helper::request_helper::add_remote_ip;
+use tardis::web::poem::Request;
+#[derive(Clone, Default)]
+pub struct IamCcAppSetApi;
+
+/// Tenant Console App Set API
+///
+#[poem_openapi::OpenApi(prefix_path = "/cc/apps", tag = "bios_basic::ApiTag::Common")]
+impl IamCcAppSetApi {
+   
+    /// Find App Tree By Current Tenant
+    ///
+    /// * Without parameters: Query the whole tree
+    /// * ``parent_sys_code=true`` : query only the next level. This can be used to query level by level when the tree is too large
+    /// * ``only_related=true`` : Invalidate the parent_sys_code parameter when this parameter is turned on, it is used to query only the tree nodes with related resources(including children nodes)
+    #[oai(path = "/tree", method = "get")]
+    async fn get_tree(
+        &self,
+        parent_sys_code: Query<Option<String>>,
+        only_related: Query<Option<bool>>,
+        ctx: TardisContextExtractor,
+        request: &Request,
+    ) -> TardisApiResult<RbumSetTreeResp> {
+        let funs = iam_constants::get_tardis_inst();
+        let ctx = IamCertServ::use_sys_or_tenant_ctx_unsafe(ctx.0)?;
+        add_remote_ip(&request, &ctx).await?;
+        let set_id = IamSetServ::get_default_set_id_by_ctx(&IamSetKind::Apps, &funs, &ctx).await?;
+        let only_related = only_related.0.unwrap_or(false);
+        let result = if only_related {
+            IamSetServ::get_tree_with_auth_by_account(&set_id, &ctx.owner, &funs, &ctx).await?
+        } else {
+            IamSetServ::get_tree(
+                &set_id,
+                &mut RbumSetTreeFilterReq {
+                    fetch_cate_item: true,
+                    hide_item_with_disabled: true,
+                    sys_codes: parent_sys_code.0.map(|parent_sys_code| vec![parent_sys_code]),
+                    sys_code_query_kind: Some(RbumSetCateLevelQueryKind::Sub),
+                    sys_code_query_depth: Some(1),
+                    ..Default::default()
+                },
+                &funs,
+                &ctx,
+            )
+            .await?
+        };
+        ctx.execute_task().await?;
+        TardisResp::ok(result)
+    }
+
+    /// Find App Set Items (App Or Account)
+    #[oai(path = "/item", method = "get")]
+    async fn find_items(
+        &self,
+        cate_id: Query<Option<String>>,
+        item_id: Query<Option<String>>,
+        ctx: TardisContextExtractor,
+        request: &Request,
+    ) -> TardisApiResult<Vec<RbumSetItemDetailResp>> {
+        let funs = iam_constants::get_tardis_inst();
+        let ctx = IamCertServ::use_sys_or_tenant_ctx_unsafe(ctx.0)?;
+        add_remote_ip(&request, &ctx).await?;
+        let set_id = IamSetServ::get_default_set_id_by_ctx(&IamSetKind::Apps, &funs, &ctx).await?;
+        let result = IamSetServ::find_set_items(Some(set_id), cate_id.0, item_id.0, None, false, &funs, &ctx).await?;
+        ctx.execute_task().await?;
+        TardisResp::ok(result)
+    }
+
+}
--- support/iam/src/iam_initializer.rs
+++ support/iam/src/iam_initializer.rs
@@ -33,7 +33,7 @@ use crate::basic::serv::iam_role_serv::IamRoleServ;
 use crate::basic::serv::iam_set_serv::IamSetServ;
 use crate::console_app::api::{iam_ca_account_api, iam_ca_app_api, iam_ca_cert_manage_api, iam_ca_res_api, iam_ca_role_api};
 use crate::console_common::api::{
-    iam_cc_account_api, iam_cc_account_task_api, iam_cc_app_api, iam_cc_config_api, iam_cc_org_api, iam_cc_res_api, iam_cc_role_api, iam_cc_system_api, iam_cc_tenant_api,
+    iam_cc_account_api, iam_cc_account_task_api, iam_cc_app_api, iam_cc_config_api, iam_cc_org_api, iam_cc_res_api, iam_cc_role_api, iam_cc_system_api, iam_cc_tenant_api, iam_cc_app_set_api,
 };
 use crate::console_interface::api::{iam_ci_account_api, iam_ci_app_api, iam_ci_cert_api, iam_ci_res_api, iam_ci_role_api, iam_ci_system_api};
 use crate::console_passport::api::{iam_cp_account_api, iam_cp_app_api, iam_cp_cert_api, iam_cp_tenant_api};
@@ -64,6 +64,7 @@ async fn init_api(web_server: &TardisWebServer) -> TardisResult<()> {
                     iam_cc_account_api::IamCcAccountApi,
                     iam_cc_account_task_api::IamCcAccountTaskApi,
                     iam_cc_app_api::IamCcAppApi,
+                    iam_cc_app_set_api::IamCcAppSetApi,
                     #[cfg(feature = "ldap_client")]
                     iam_cc_account_api::IamCcAccountLdapApi,
                     iam_cc_role_api::IamCcRoleApi,
