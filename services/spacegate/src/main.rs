25ed231525d79a93f0200a73dac1da02482f930b
RWDai <27391645+RWDai@users.noreply.github.com>
2023-10-27 16:33:15 +0800

    set-gateway-default-log-level-info

--- services/spacegate/src/main.rs
+++ services/spacegate/src/main.rs
@@ -2,11 +2,12 @@ use spacegate_lib::register_lib_filter;
 use tardis::basic::result::TardisResult;
 use tardis::{tokio, TardisFuns};
 use tardis::config::config_dto::{FrameworkConfig, LogConfig, TardisConfig};
+use tracing_subscriber::filter::Directive;
 
 #[tokio::main]
 async fn main() -> TardisResult<()> {
     TardisFuns::init_log()?;
-    TardisFuns::init_conf(TardisConfig::builder().fw(FrameworkConfig::builder().log(LogConfig::default()).build()).build()).await?;
+    TardisFuns::init_conf(TardisConfig::builder().fw(FrameworkConfig::builder().log(LogConfig::builder().level("info".parse::<Directive>().expect("parse info level error!")).build()).build()).build()).await?;
     let namespaces = std::env::args().nth(1).map(Some).unwrap_or(None);
     register_lib_filter();
     spacegate_kernel::startup(true, namespaces, None).await?;
