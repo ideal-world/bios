a7de25e2cc329501a12ae28c3452e2f478e039aa
ljl <17743125563@163.com>
2023-10-28 10:02:30 +0800

    spi-search:fix coalesce empty.

--- spi/spi-search/src/serv/pg/search_pg_item_serv.rs
+++ spi/spi-search/src/serv/pg/search_pg_item_serv.rs
@@ -1148,7 +1148,7 @@ pub async fn query_metrics(query_req: &SearchQueryMetricsReq, funs: &TardisFunsI
     // (column name with fun, alias name, show_name, is dimension)
     let mut sql_part_outer_select_infos = vec![];
     for (column_name_with_fun, alias_name, show_name) in sql_part_group_infos {
-        sql_part_outer_select_infos.push((format!("COALESCE({},'empty')",column_name_with_fun), alias_name, show_name, true));
+        sql_part_outer_select_infos.push((format!("COALESCE({},'\"empty\"')",column_name_with_fun), alias_name, show_name, true));
     }
     for select in &query_req.select {
         let select_column = if select.in_ext.unwrap_or(true) {
