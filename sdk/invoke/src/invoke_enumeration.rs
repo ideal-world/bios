4323f58abafe43c32dca9c89d13927a4f3aff755
ZzIsGod1019 <1498852723@qq.com>
2024-03-08 01:09:04 -0800

    reach: fix missing iam base url

--- sdk/invoke/src/invoke_enumeration.rs
+++ sdk/invoke/src/invoke_enumeration.rs
@@ -3,10 +3,9 @@ use std::fmt::Display;
 use serde::{Deserialize, Serialize};
 #[cfg(feature = "reldb-core")]
 use tardis::db::sea_orm;
-use tardis::web::poem_openapi;
+use tardis::{web::poem_openapi};
 
 #[derive(Clone, Debug, PartialEq, Eq, Deserialize, Serialize, poem_openapi::Enum)]
-#[serde(rename_all = "lowercase")]
 #[cfg_attr(feature = "reldb-core", derive(strum::EnumString))]
 pub enum InvokeModuleKind {
     #[oai(rename = "search")]
@@ -33,7 +32,7 @@ pub enum InvokeModuleKind {
     Event,
 }
 
-impl std::fmt::Display for InvokeModuleKind {
+impl Display for InvokeModuleKind {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             InvokeModuleKind::Search => write!(f, "search"),
