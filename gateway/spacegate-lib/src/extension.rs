63f23f3500e27206171190ef0b624f16b4c4610a
RWDai <27391645+RWDai@users.noreply.github.com>
2024-04-01 03:18:41 +0000

    test

--- gateway/spacegate-lib/src/extension.rs
+++ gateway/spacegate-lib/src/extension.rs
@@ -1,4 +1,22 @@
+use self::audit_log_param::{AuditLogParam, LogParamContent};
+
 pub mod audit_log_param;
 pub mod before_encrypt_body;
 pub mod cert_info;
 pub mod request_crypto_status;
+
+pub enum ExtensionPackEnum {
+    AuditLogParam(AuditLogParam),
+    LogParamContent(LogParamContent),
+    None,
+}
+
+impl From<String> for ExtensionPackEnum {
+    fn from(value: String) -> Self {
+        match value.as_str() {
+            "log_param" => ExtensionPackEnum::AuditLogParam(AuditLogParam::new()),
+            "log_content" => ExtensionPackEnum::LogParamContent(LogParamContent::new()),
+            _ => ExtensionPackEnum::None,
+        }
+    }
+}
--- gateway/spacegate-lib/src/extension/audit_log_param.rs
+++ gateway/spacegate-lib/src/extension/audit_log_param.rs
@@ -1,6 +1,9 @@
-use spacegate_shell::hyper::HeaderMap;
+use serde::{Deserialize, Serialize};
+use spacegate_shell::{hyper::HeaderMap, kernel::extension::ExtensionPack};
 
-#[derive(Clone)]
+use super::cert_info::RoleInfo;
+
+#[derive(Clone, Serialize, Deserialize)]
 pub struct AuditLogParam {
     pub request_path: String,
     pub request_method: String,
@@ -8,3 +11,23 @@ pub struct AuditLogParam {
     pub request_scheme: String,
     pub request_ip: String,
 }
+
+impl ExtensionPack for AuditLogParam {}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct LogParamContent {
+    pub op: String,
+    pub name: String,
+    pub user_id: Option<String>,
+    pub role: Vec<RoleInfo>,
+    pub ip: String,
+    pub path: String,
+    pub scheme: String,
+    pub token: Option<String>,
+    pub server_timing: Option<String>,
+    pub resp_status: String,
+    //Indicates whether the business operation was successful.
+    pub success: bool,
+}
+
+impl ExtensionPack for LogParamContent {}
