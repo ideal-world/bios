d9fd7847ca01fda858a6409f379d62f2174515eb
ljl <17743125563@163.com>
2024-05-09 09:44:09 +0800

    event:fix

--- backend/middlewares/event/src/api/event_proc_api.rs
+++ backend/middlewares/event/src/api/event_proc_api.rs
@@ -8,11 +8,13 @@ use crate::serv::event_proc_serv;
 pub struct EventProcApi;
 
 /// Event Process API
+/// 
 /// 事件处理API
 #[poem_openapi::OpenApi(prefix_path = "/proc")]
 impl EventProcApi {
 
     /// Process event
+    /// 
     /// 处理事件
     #[oai(path = "/:listener_code", method = "get")]
     async fn ws_process(&self, listener_code: Path<String>, token: Query<String>, websocket: WebSocket) -> BoxWebSocketUpgraded {
