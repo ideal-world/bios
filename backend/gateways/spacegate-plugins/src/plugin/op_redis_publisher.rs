61adf82a213c3c59b79ff1653d06e08d11a80b20
ZzIsGod1019 <1498852723@qq.com>
2024-05-13 15:20:39 +0800

    update-gateway-plugin-to-dev-branch

--- backend/gateways/spacegate-plugins/src/plugin/op_redis_publisher.rs
+++ backend/gateways/spacegate-plugins/src/plugin/op_redis_publisher.rs
@@ -42,8 +42,14 @@ pub struct RedisPublisherPlugin {
 impl Plugin for RedisPublisherPlugin {
     const CODE: &'static str = "op_redis_publisher";
 
+    fn meta() -> spacegate_plugin::PluginMetaData {
+        spacegate_plugin::plugin_meta!(
+            description: "Build for open platform, and it depend on plugin audit-log"
+        )
+    }
+
     fn create(config: PluginConfig) -> Result<Self, BoxError> {
-        let id = config.none_mono_id();
+        let id = config.id;
         let layer_config = serde_json::from_value::<RedisPublisherConfig>(config.spec.clone())?;
 
         Ok(Self {
