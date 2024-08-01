0668f4d9d75f506f2a96e8234760bbdfd150ebb8
RWDai <27391645+RWDai@users.noreply.github.com>
2024-08-01 14:05:23 +0800

    feat: spi os add obs (#811)

---
+++ backend/spi/spi-object/src/serv/obs/object_obs_obj_serv.rs
@@ -0,0 +1,16 @@
+use tardis::{
+    basic::{result::TardisResult},
+    os::{
+        os_client::TardisOSClient,
+    },
+};
+
+use crate::serv::s3::S3;
+
+/// OBS need manually configure lifecycle rules
+pub(crate) struct OBSService;
+impl S3 for OBSService {
+    async fn rebuild_path(_bucket_name: Option<&str>, origin_path: &str, _obj_exp: Option<u32>, _client: &TardisOSClient) -> TardisResult<String> {
+        Ok(origin_path.to_string())
+    }
+}
