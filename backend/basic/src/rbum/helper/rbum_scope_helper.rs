cbd5eab190a37b37014569d6d50912d345223bb7
ljl <17743125563@163.com>
2024-08-05 14:10:01 +0800

    fix locale base.

--- backend/basic/src/rbum/helper/rbum_scope_helper.rs
+++ backend/basic/src/rbum/helper/rbum_scope_helper.rs
@@ -255,7 +255,7 @@ where
 #[cfg(feature = "default")]
 pub fn check_without_owner_and_unsafe_fill_ctx(request: &tardis::web::poem::Request, funs: &TardisFunsInst, ctx: &mut TardisContext) -> TardisResult<()> {
     if ctx.ak.is_empty() {
-        return Err(TardisError::forbidden("[Basic] Request context owner is not empty", "403-rbum-req-ctx-owner-is-not-empty"));
+        return Err(TardisError::forbidden("[Basic] Request contex ak is not empty", "403-rbum-req-ctx-ak-is-not-empty"));
     }
     unsafe_fill_ctx(request, funs, ctx)
 }
