ea266d15f1464e03f682742fbc036d0899484fac
RWDai <27391645+RWDai@users.noreply.github.com>
2024-11-01 11:30:20 +0800

    fix cicd

--- backend/spi/spi-stats/src/serv/stats_sync_serv.rs
+++ backend/spi/spi-stats/src/serv/stats_sync_serv.rs
@@ -14,12 +14,12 @@ spi_dispatch_service! {
   @init: stats_initializer::init_fun,
   @dispatch: {
       #[cfg(feature = "spi-pg")]
-      spi_constants::SPI_PG_KIND_CODE => pg::stats_pg_record_serv,
+      spi_constants::SPI_PG_KIND_CODE => pg::stats_pg_sync_serv,
   },
   @method: {
     fact_record_sync(fact_key: &str) -> TardisResult<()>;
     fact_col_record_sync(fact_key: &str, col_key: &str) -> TardisResult<()>;
-    db_config_add(add_req: StatsSyncDbConfigAddReq) -> TardisResult<()>;
+    db_config_add(add_req: StatsSyncDbConfigAddReq) -> TardisResult<String>;
     db_config_modify(modify_req: StatsSyncDbConfigModifyReq) -> TardisResult<()>;
     db_config_list() -> TardisResult<Vec<StatsSyncDbConfigInfoResp>>;
   }
