d73172043234d016a5678afe1577ec118adfc3d8
4t145 <u4t145@163.com>
2025-01-22 14:53:09 +0800

    disable event in bios-all

--- backend/services/bios-all/src/initializer.rs
+++ backend/services/bios-all/src/initializer.rs
@@ -3,7 +3,7 @@ use tardis::basic::result::TardisResult;
 use tardis::web::web_server::TardisWebServer;
 
 pub async fn init(web_server: &TardisWebServer) -> TardisResult<()> {
-    bios_mw_event::event_initializer::init(web_server).await?;
+    // bios_mw_event::event_initializer::init(web_server).await?;
 
     bios_auth::auth_initializer::init(web_server).await?;
     bios_iam::iam_initializer::init(web_server).await?;
