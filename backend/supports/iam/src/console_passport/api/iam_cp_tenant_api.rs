e0479fb8918cc17e4cf40b3d4a76a85ca5a3a3fc
ZzIsGod1019 <1498852723@11.com>
2025-07-09 11:22:17 +0800

    fix

--- backend/supports/iam/src/console_passport/api/iam_cp_tenant_api.rs
+++ backend/supports/iam/src/console_passport/api/iam_cp_tenant_api.rs
@@ -5,7 +5,7 @@ use bios_basic::rbum::dto::rbum_filer_dto::RbumBasicFilterReq;
 use bios_basic::rbum::serv::rbum_item_serv::RbumItemCrudOperation;
 
 use crate::basic::dto::iam_filer_dto::IamTenantFilterReq;
-use crate::basic::dto::iam_tenant_dto::{IamTenantBoneResp, IamTenantKeyNameResp};
+use crate::basic::dto::iam_tenant_dto::IamTenantBoneResp;
 use crate::basic::serv::iam_cert_serv::IamCertServ;
 use crate::basic::serv::iam_tenant_serv::IamTenantServ;
 use crate::iam_constants;
@@ -44,38 +44,6 @@ impl IamCpTenantApi {
                 id: i.id,
                 name: i.name,
                 icon: i.icon,
-            })
-            .collect();
-        TardisResp::ok(result)
-    }
-
-    /// Get All Tenants Return Kv
-    /// 查找租户
-    #[oai(path = "/all/key-name", method = "get")]
-    async fn key_name(&self) -> TardisApiResult<Vec<IamTenantKeyNameResp>> {
-        let funs = iam_constants::get_tardis_inst();
-        let result = IamTenantServ::find_items(
-            &IamTenantFilterReq {
-                basic: RbumBasicFilterReq {
-                    ignore_scope: true,
-                    own_paths: Some("".to_string()),
-                    with_sub_own_paths: true,
-                    enabled: Some(true),
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            None,
-            None,
-            &funs,
-            &IamCertServ::get_anonymous_context(),
-        )
-        .await?;
-        let result = result
-            .into_iter()
-            .map(|i| IamTenantKeyNameResp {
-                key: i.id,
-                name: i.name,
             })
             .collect();
         TardisResp::ok(result)
