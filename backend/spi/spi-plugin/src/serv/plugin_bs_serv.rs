65b0a8b88f9e4b390b8ea86783f56760c5b46ca0
ljl <17743125563@163.com>
2025-09-10 10:45:13 +0800

    spi-plugin:fix kv.

--- backend/spi/spi-plugin/src/serv/plugin_bs_serv.rs
+++ backend/spi/spi-plugin/src/serv/plugin_bs_serv.rs
@@ -74,7 +74,7 @@ impl PluginBsServ {
                         )
                         .await;
                         let _ = SpiKvClient::add_or_modify_key_name(
-                            &format!("{}{}", &funs.conf::<PluginConfig>().kv_plugin_prefix.clone(), rel_id_clone),
+                            &format!("{}:{}", &funs.conf::<PluginConfig>().kv_plugin_prefix.clone(), rel_id_clone),
                             &rel_name_clone,
                             None,
                             &funs,
@@ -139,7 +139,7 @@ impl PluginBsServ {
                         )
                         .await;
                         let _ = SpiKvClient::add_or_modify_key_name(
-                            &format!("{}{}", &funs.conf::<PluginConfig>().kv_plugin_prefix.clone(), rel_id_clone),
+                            &format!("{}:{}", &funs.conf::<PluginConfig>().kv_plugin_prefix.clone(), rel_id_clone),
                             &rel_name_clone,
                             None,
                             &funs,
