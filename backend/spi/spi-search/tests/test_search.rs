0c6522efad39c1781c1006080e6622fe549acff1
ZzIsGod1019 <1498852723@qq.com>
2025-12-03 20:07:07 +0800

    fix test

--- backend/spi/spi-search/tests/test_search.rs
+++ backend/spi/spi-search/tests/test_search.rs
@@ -20,7 +20,9 @@ mod test_search_item;
 
 #[tokio::test]
 async fn test_search() -> TardisResult<()> {
-    env::set_var("RUST_LOG", "debug,test_search=trace,sqlx::query=off");
+    unsafe {
+        env::set_var("RUST_LOG", "debug,test_search=trace,sqlx::query=off");
+    }
 
     let _x = init_search_container::init().await?;
     // init_data(spi_constants::SPI_ES_KIND_CODE, &env::var("TARDIS_FW.ES.URL").unwrap(), false).await?;
