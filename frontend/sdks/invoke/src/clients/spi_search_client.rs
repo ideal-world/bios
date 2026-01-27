54bb1658b2b562826226bc9e2ac3d8f9caf9b3d4
ljl <17743125563@163.com>
2026-01-27 14:20:14 +0800

    iam:fix sub deploy and search_client add save.

--- frontend/sdks/invoke/src/clients/spi_search_client.rs
+++ frontend/sdks/invoke/src/clients/spi_search_client.rs
@@ -5,7 +5,9 @@ use tardis::TardisFunsInst;
 
 #[cfg(feature = "event")]
 use crate::clients::event_client::mq_client_node;
-use crate::dto::search_item_dto::{SearchEventItemDeleteReq, SearchEventItemModifyReq, SearchItemAddReq, SearchItemModifyReq, SearchItemSearchReq, SearchItemSearchResp, SearchSaveItemReq};
+use crate::dto::search_item_dto::{
+    SearchEventItemDeleteReq, SearchEventItemModifyReq, SearchItemAddReq, SearchItemModifyReq, SearchItemSearchReq, SearchItemSearchResp, SearchSaveItemReq,
+};
 #[cfg(feature = "event")]
 use crate::invoke_config::InvokeConfigApi as _;
 use crate::invoke_enumeration::InvokeModuleKind;
@@ -108,6 +110,17 @@ impl SpiSearchClient {
         let resp =
             funs.web_client().put::<SearchItemSearchReq, TardisResp<TardisPage<SearchItemSearchResp>>>(format!("{search_url}/ci/item/search"), search_req, headers.clone()).await?;
         BaseSpiClient::package_resp(resp)
+    }
+
+    pub async fn save(tag: &str, save_req: &SearchSaveItemReq, name: Option<String>, kv_disable: Option<bool>, funs: &TardisFunsInst, ctx: &TardisContext) -> TardisResult<()> {
+        let search_url = BaseSpiClient::module_url(InvokeModuleKind::Search, funs).await?;
+        let headers = BaseSpiClient::headers(None, funs, ctx).await?;
+        funs.web_client().put_obj_to_str(&format!("{search_url}/ci/item/{tag}/save"), save_req, headers.clone()).await?;
+        if save_req.title.is_some() || name.is_some() {
+            let name = name.clone().unwrap_or(save_req.title.clone().unwrap_or("".to_string()));
+            SpiKvClient::add_or_modify_key_name(&format!("{tag}:{}", save_req.key), &name, kv_disable, funs, ctx).await?;
+        }
+        Ok(())
     }
 
     /// Batch save
